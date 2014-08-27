4f3d43b64002dbb2bb8288d75dbc963f2e311812
loki2302 <loki2302@loki2302.me>
2014-08-27 23:35:19 +0400

    Updated express-webservice-experiment to Express 4.x, cleaned up

--- express-webservice-experiment/app.js
+++ express-webservice-experiment/app.js
@@ -1,55 +1,58 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var expressValidator = require("express-validator");
 
-app = express();
+var app = express();
+app.use(bodyParser.json());
+app.use(expressValidator());
 
-app.use(express.bodyParser());
-app.use(expressValidator);
-
-app.get("/api/addNumbers/", function(request, response) {
-  request.assert("a", "Invalid a").isInt();
-  request.assert("b", "Invalid b").isInt();
-
-  var errors = request.validationErrors();
+// GET http://localhost:3000/api/addNumbers/?a=2&b=3
+app.get("/api/addNumbers/", function(req, res) {
+  req.assert("a", "Invalid a").isInt();
+  req.assert("b", "Invalid b").isInt();
+  
+  var errors = req.validationErrors();
   if(!!errors) {
     console.log("errors:");
     console.log(errors);
-    response.send(errors);
+    res.send(errors);
     return;
   }
 
-  var a = request.sanitize("a").toInt();
-  var b = request.sanitize("b").toInt();
+  var a = req.sanitize("a").toInt();
+  var b = req.sanitize("b").toInt();
 
   var result = {
     "result": a + b,
     "message": "processed GET-request"
   };
 
-  response.send(result);
+  res.send(result);
 });
 
-app.post("/api/addNumbers/", function(request, response) {
-  request.checkBody("a", "Invalid a").isInt();
-  request.checkBody("b", "Invalid b").isInt();
+// POST http://localhost:3000/api/addNumbers/
+// { "a": 2, "b": 3 }
+app.post("/api/addNumbers/", function(req, res) {
+  req.checkBody("a", "Invalid a").isInt();
+  req.checkBody("b", "Invalid b").isInt();
 
-  var errors = request.validationErrors();
+  var errors = req.validationErrors();
   if(!!errors) {
     console.log("errors:");
     console.log(errors);
-    response.send(errors);
+    res.send(errors);
     return;
   }
 
-  var a = request.sanitize("a").toInt();
-  var b = request.sanitize("b").toInt();
+  var a = req.sanitize("a").toInt();
+  var b = req.sanitize("b").toInt();
 
   var result = {
     "result": a + b,
     "message": "processed POST-request"
   };
 
-  response.send(result);
+  res.send(result);
 });
 
-app.listen(8080);
+app.listen(3000);
