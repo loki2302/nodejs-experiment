cf6c2fb8901860a5d69343b8fe57ee9f3e2f7f4f
loki2302 <loki2302@loki2302.me>
2014-08-29 00:05:50 +0400

    Added more complicated async test to mocha-experiment's async playground

--- mocha-experiment/test/playingWithAsync.js
+++ mocha-experiment/test/playingWithAsync.js
@@ -8,3 +8,27 @@ describe("1 + 2", function() {
 		}, 500);
 	});
 });
+
+describe("a weird approach to computing 1 + 2", function() {
+	var one = 0;
+	before(function(done) {
+		setTimeout(function() {
+			one = one + 5;
+			done();
+		}, 500);
+	});
+
+	beforeEach(function(done) {
+		setTimeout(function() {
+			one = one - 4;
+			done();
+		}, 500);
+	});
+
+	it("should work", function(done) {
+		setTimeout(function() {
+			assert.equal(3, one + 2);
+			done();
+		}, 500);
+	});
+});
