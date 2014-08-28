6ce7ffcb09a7903ad4ffe63815ad5c9d99e60b2a
loki2302 <loki2302@loki2302.me>
2014-08-28 22:58:49 +0400

    Extracted DAO, divided one large test into few smaller ones

---
+++ sequelize-experiment/dao.js
@@ -0,0 +1,47 @@
+var Sequelize = require("sequelize");
+
+var DAO = function() {
+	this.sequelize = new Sequelize('database', 'username', 'password', {
+	  dialect: 'sqlite',
+	  storage: 'my.db'
+	});
+
+	this.Note = this.sequelize.define('Note', {
+	  title: Sequelize.STRING,
+	  description: Sequelize.TEXT
+	});
+};
+
+DAO.prototype.initialize = function(success, error) {
+	this.sequelize.sync().success(function() {
+		success();
+	}).error(function(e) {
+		error(e);
+	});
+};
+
+DAO.prototype.drop = function(success, error) {
+	this.sequelize.drop().success(function() {
+		success();
+	}).error(function(e) {
+		error(e);
+	});
+};
+
+DAO.prototype.getAllNotes = function(success, error) {
+	this.Note.all().success(function(notes) {
+		success(notes);
+	}).error(function(e) {
+		error(e);
+	});
+};
+
+DAO.prototype.createNote = function(fields, success, error) {	
+	this.Note.create(fields).success(function(note) {
+		success(note);
+	}).error(function(e) {
+		error(e);
+	});
+};
+
+exports.DAO = DAO;
--- sequelize-experiment/tests.js
+++ sequelize-experiment/tests.js
@@ -1,41 +1,62 @@
-var Sequelize = require("sequelize");
+var DAO = require("./dao.js").DAO;
 
-exports.basicUseCase = function(test) {
-	var sequelize = new Sequelize('database', 'username', 'password', {
-	  dialect: 'sqlite',
-	  storage: 'my.db'
+exports.setUp = function(callback) {
+	this.dao = new DAO();
+	this.dao.initialize(function() {
+		callback();
+	}, function() {
+		throw "initialize() failed";
 	});
+};
 
-	var Note = sequelize.define('Note', {
-	  title: Sequelize.STRING,
-	  description: Sequelize.TEXT
+exports.tearDown = function(callback) {
+	this.dao.drop(function() {
+		callback();
+	}, function() {
+		throw "drop() failed";
 	});
+};
 
-	sequelize.sync().success(function() {
-		Note.all().success(function(notes) {
-			test.equal(notes.length, 0);
-
-			Note.create({ title: "title 1", description: "description 1"}).success(function(note) {
-				test.equal(note.id, 1);
-				test.equal(note.title, "title 1");
-				test.equal(note.description, "description 1");
-
-				sequelize.drop().success(function() {
-					test.done();
-				}).error(function(error) {
-					test.ok(false, "drop() failed");
-					test.done();
-				});
-			}).error(function(error) {
-				test.ok(false, "Note.create() failed");
-				test.done();
-			});
-		}).error(function(error) {
-			test.ok(false, "Note.all() failed");
-			test.done();
-		});	
-	}).error(function(error) {		
-		test.ok(false, "sync() failed");
+exports.thereAreNoNotesByDefault = function(test) {
+	var dao = this.dao;
+	dao.getAllNotes(function(notes) {
+		test.equal(notes.length, 0);
 		test.done();
+	}, function() {
+		test.ok(false, "getAllNotes() failed");
 	});
+};
+
+exports.canCreateNote = function(test) {
+	var dao = this.dao;
+	var fields = {
+		title: "title 1", 
+		description: "description 1"
+	};
+	dao.createNote(fields, function(note) {
+		test.equal(note.id, 1);
+		test.equal(note.title, "title 1");
+		test.equal(note.description, "description 1");
+		test.done();
+	}, function() {
+		test.ok(false, "createNote() failed");
+	});
+};
+
+exports.thereIsExactlyOneNoteAfterOneNoteIsCreated = function(test) {
+	var dao = this.dao;
+	var fields = {
+		title: "title 1", 
+		description: "description 1"
+	};
+	dao.createNote(fields, function(note) {
+		dao.getAllNotes(function(notes) {
+			test.equal(notes.length, 1);
+			test.done();
+		}, function() {
+			test.ok(false, "getAllNotes() failed");
+		});
+	}, function() {
+		test.ok(false, "createNote() failed");
+	});	
 };
