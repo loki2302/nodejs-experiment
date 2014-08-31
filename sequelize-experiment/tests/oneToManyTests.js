90ec5661583e8498a4d0aab167fcf08d51181842
loki2302 <loki2302@loki2302.me>
2014-08-31 13:30:57 +0400

    Cleaning up sequelize-experiment

---
+++ sequelize-experiment/tests/oneToManyTests.js
@@ -0,0 +1,92 @@
+var Sequelize = require("sequelize");
+var Q = require("q");
+
+exports.oneToManyTests = {
+	setUp: function(callback) {
+		this.sequelize = new Sequelize('database', 'username', 'password', {
+			dialect: 'sqlite',
+			storage: 'my.db'
+		});
+
+		this.Note = this.sequelize.define('Note', {
+			content: Sequelize.STRING
+		});
+
+		this.Category = this.sequelize.define('Category', {
+			name: Sequelize.STRING
+		});
+
+		this.Category.hasMany(this.Note);
+		this.Note.belongsTo(this.Category);
+
+		this.sequelize.sync().success(function() {
+			callback();
+		}).error(function(e) {
+			throw e;
+		});
+	},
+
+	tearDown: function(callback) {
+		this.sequelize.drop().success(function() {
+			callback()
+		}).error(function(e) {
+			throw e;
+		});
+	},
+
+	dummy: function(test) {
+		var self = this;
+		function createCategory(name) {
+			var deferred = Q.defer();
+
+			self.Category.create({ name: name }).success(function(category) {
+				deferred.resolve(category);
+			}).error(function(e) {
+				deferred.reject(e);
+			});
+
+			return deferred.promise;
+		}
+
+		function createNote(categoryId, content) {
+			var deferred = Q.defer();
+
+			self.Note.create({ content: content, CategoryId: categoryId }).success(function(note) {
+				deferred.resolve(note);
+			}).error(function(e) {
+				deferred.reject(e);
+			});
+
+			return deferred.promise;
+		}
+
+		function getNoteWithCategory(noteId) {
+			var deferred = Q.defer();
+
+			var options = { 
+				where: { id: noteId }, 
+				include: [ self.Category ]
+			};
+			self.Note.find(options).success(function(note) {
+				deferred.resolve(note);
+			}).error(function(e) {
+				deferred.reject(e);
+			});
+
+			return deferred.promise;
+		}
+
+		createCategory("js")
+		.then(function(jsCategory) {
+			return createNote(jsCategory.id, "hello");
+		})
+		.then(function(note) {
+			var noteId = note.id;
+			return getNoteWithCategory(noteId);
+		})
+		.then(function(note) {
+			console.log(JSON.stringify(note));
+			test.done();
+		});
+	}
+};
