8c7a41f2bb3a986bee8c7a816711569642b52eee
loki2302 <loki2302@loki2302.me>
2014-09-10 10:43:39 +0400

    Added tagged note validator test

--- jsonschema-experiment/tests.js
+++ jsonschema-experiment/tests.js
@@ -105,3 +105,73 @@ describe("Note validator", function() {
 		assert.equal(result.errors.length, 1);
 	});
 });
+
+describe("Tagged note validator", function() {
+	var schema;
+	before(function() {
+		schema = {
+			type: "object",
+			properties: {
+				id: {
+					type: "integer",
+					required: true
+				},
+				content: {
+					type: "string",
+					required: true
+				},
+				tags: {
+					type: "array",
+					required: false,
+					items: {
+						type: "object",
+						properties: {
+							id: {
+								type: "integer",
+								required: true
+							},
+							name: {
+								type: "string",
+								required: false
+							}
+						}
+					}
+				}
+			}
+		};
+	});
+
+	it("should be OK when tags are there and they have names", function() {
+		var result = validate({
+			id: 123,
+			content: "hello",
+			tags: [
+				{ id: 111, name: "tag one" },
+				{ id: 222, name: "tag two" },
+				{ id: 333, name: "tag three" }
+			]
+		}, schema);
+		assert.equal(result.errors.length, 0);
+	});
+
+	it("should be OK when tags are there and they don't have names", function() {
+		var result = validate({
+			id: 123,
+			content: "hello",
+			tags: [
+				{ id: 111 },
+				{ id: 222 },
+				{ id: 333 }
+			]
+		}, schema);
+		assert.equal(result.errors.length, 0);
+	});
+
+	it("should be OK when tags are not there", function() {
+		var result = validate({
+			id: 123,
+			content: "hello"
+		}, schema);
+		assert.equal(result.errors.length, 0);
+	});
+});
