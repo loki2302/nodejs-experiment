981f9e07562b119ceba3529dad4313c0259e345a
loki2302 <loki2302@loki2302.me>
2014-09-10 09:45:49 +0400

    Added dummy transaction test to sequelize

---
+++ sequelize-experiment/tests/transactionTests.js
@@ -0,0 +1,78 @@
+var Sequelize = require("sequelize");
+
+exports.transactionTests = {
+	setUp: function(callback) {
+		this.sequelize = new Sequelize('database', 'username', 'password', {
+			dialect: 'sqlite',
+			storage: 'my.db'
+		});
+
+		this.Note = this.sequelize.define('Note', {			
+			content: Sequelize.TEXT
+		});
+
+		this.sequelize.sync().success(function() {
+			callback();
+		}).error(function(e) {			
+			throw e;
+		});
+	},
+
+	tearDown: function(callback) {
+		this.sequelize.drop().success(function() {
+			callback()
+		}).error(function(e) {
+			throw e;
+		});
+	},
+
+	dummy: function(test) {
+		var self = this;
+		self.sequelize.transaction({
+			isolationLevel: "READ UNCOMMITTED"
+		}, function(tx) {
+			// create note in transaction tx
+			self.Note.create({ 
+				content: "hello" 
+			}, {
+				transaction: tx
+			}).success(function(note) {
+				// make sure note is visible inside tx
+				self.Note.find(1, { 
+					transaction: tx 
+				}).success(function(note) {		
+					if(!note) {
+						test.ok(false);
+						return;
+					}
+
+					// make sure note is invisible outside tx
+					self.Note.find(1).success(function(note) {
+						if(!!note) {
+							test.ok(false);
+							return;
+						}
+
+						tx.commit().success(function() {
+							// make sure note is visible outside tx after commit
+							self.Note.find(1).success(function(note) {
+								test.ok(!!note);
+								test.done();
+							}).error(function(e) {
+								test.ok(false);
+							});							
+						}).error(function(e) {
+							test.ok(false);
+						});
+					}).error(function(e) {
+						test.ok(false);
+					});					
+				}).error(function(e) {
+					test.ok(false);
+				});
+			}).error(function(e) {
+				test.ok(false);
+			});
+		});		
+	}
+};
