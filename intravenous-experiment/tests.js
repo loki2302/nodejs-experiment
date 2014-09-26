f1407f6779db9f2fff368917463edb672460abb5
loki2302 <loki2302@loki2302.me>
2014-09-26 23:33:07 +0400

    Added more tests to intravenous-experiment

--- intravenous-experiment/tests.js
+++ intravenous-experiment/tests.js
@@ -1,7 +1,87 @@
 var intravenous = require("intravenous");
 
 module.exports = {
-	"I can use intravenous": function(test) {
+	"I can register a class and a new instance on each request": function(test) {
+		var instanceCount = 0;
+		function Service() {
+			++instanceCount;
+		};
+
+		var container = intravenous.create();
+		container.register("service", Service);
+		
+		var service1 = container.get("service");
+		test.ok(service1 instanceof Service);
+		test.equal(instanceCount, 1);
+
+		var service2 = container.get("service");
+		test.ok(service2 instanceof Service);
+		test.equal(instanceCount, 2);
+
+		test.done();
+	},
+
+	"I can register a specific instance and get it on each request": function(test) {
+		var instanceCount = 0;
+		function Service() {
+			++instanceCount;
+		};
+
+		var container = intravenous.create();		
+		container.register("service", new Service());
+
+		var service1 = container.get("service");
+		test.ok(service1 instanceof Service);
+		test.equal(instanceCount, 1);
+
+		var service2 = container.get("service");
+		test.ok(service2 instanceof Service);
+		test.equal(instanceCount, 1);
+
+		test.done();
+	},
+
+	"I can register a class as a singleton": function(test) {
+		var instanceCount = 0;
+		function Service() {
+			++instanceCount;
+		};
+
+		var container = intravenous.create();		
+		container.register("service", Service, "singleton");
+
+		var service1 = container.get("service");
+		test.ok(service1 instanceof Service);
+		test.equal(instanceCount, 1);
+
+		var service2 = container.get("service");
+		test.ok(service2 instanceof Service);
+		test.equal(instanceCount, 1);
+
+		test.done();
+	},
+
+	"I can register a class not as singleton": function(test) {
+		var instanceCount = 0;
+		function Service() {
+			++instanceCount;
+		};
+
+		var container = intravenous.create();
+		container.register("service", Service, "unique");
+		
+		var service1 = container.get("service");
+		test.ok(service1 instanceof Service);
+		test.equal(instanceCount, 1);
+
+		var service2 = container.get("service");
+		test.ok(service2 instanceof Service);
+		test.equal(instanceCount, 2);
+
+		test.done();
+	},
+
+	"I can have stuff injected": function(test) {
 		function MyDAO() {};
 
 		MyDAO.prototype.getData = function() {
