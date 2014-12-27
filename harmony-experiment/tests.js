a5eb5f41def6424f4516e27017977d73449860b1
loki2302 <loki2302@loki2302.me>
2014-12-27 14:41:14 +0300

    Added yield/q exception test

--- harmony-experiment/tests.js
+++ harmony-experiment/tests.js
@@ -23,7 +23,7 @@ describe('harmony', function() {
 			assert.equal(v.done, true);
 		});
 
-		it('should let me make promises look synchronous', function(done) {
+		it('should let me get promise result synchronously', function(done) {
 			function getData() {
 				var deferred = Q.defer();
 				setTimeout(function() {
@@ -38,11 +38,36 @@ describe('harmony', function() {
 				done();
 			}
 
-			var g = controllerFunc();
-			
+			var g = controllerFunc();			
 			var v = g.next();
 			v.value.then(function(result) {
 				g.next(result);
+			});
+		});
+
+		it('should let me get promise rejection as exception', function(done) {
+			function getData() {
+				var deferred = Q.defer();
+				setTimeout(function() {
+					deferred.reject(new Error('no data'));
+				}, 100);
+				return deferred.promise;
+			}			
+
+			function* controllerFunc() {
+				try {
+					var data = yield getData();
+					assert.ok(false);
+				} catch(e) {
+					assert.equal(e.message, 'no data');
+					done();
+				}				
+			}
+
+			var g = controllerFunc();			
+			var v = g.next();
+			v.value.then(undefined, function(error) {
+				g.throw(error);
 			});
 		});
 	});	
