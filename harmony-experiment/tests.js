1cff916b9de748f3c33ba2c0efe86ad31b82a499
loki2302 <loki2302@loki2302.me>
2014-12-27 14:57:12 +0300

    Added harmony/co tests

--- harmony-experiment/tests.js
+++ harmony-experiment/tests.js
@@ -1,7 +1,58 @@
 var assert = require('assert');
 var Q = require('q');
+var co = require('co');
 
 describe('harmony', function() {
+	describe('co', function() {
+		it('should let me make successful synchronous calls', function(done) {
+			var getDataCallCount = 0;
+			function getData(x) {
+				++getDataCallCount;
+
+				var deferred = Q.defer();
+				setTimeout(function() {
+					deferred.resolve('data' + x);
+				}, 1);
+				return deferred.promise;
+			}
+
+			co(function* () {
+				var data1 = yield getData(1);
+				var data2 = yield getData(2);
+				var data3 = yield getData(3);
+				return data1 + data2 + data3;
+			}).then(function(result) {
+				assert.equal(result, 'data1data2data3');
+				assert.equal(getDataCallCount, 3);
+				done();
+			});
+		});
+
+		it('should let me make unsuccessful synchronous calls', function(done) {
+			var getDataCallCount = 0;
+			function getData(x) {
+				++getDataCallCount;
+
+				var deferred = Q.defer();
+				setTimeout(function() {
+					deferred.reject(new Error('error' + x));
+				}, 1);
+				return deferred.promise;
+			}
+
+			co(function* () {
+				var data1 = yield getData(1);
+				var data2 = yield getData(2);
+				var data3 = yield getData(3);
+				return data1 + data2 + data3;
+			}).then(undefined, function(error) {
+				assert.equal(error.message, 'error1');
+				assert.equal(getDataCallCount, 1);
+				done();
+			});
+		});
+	});
+
 	describe('generator', function() {
 		it('should work', function() {
 			function* hiThereGenerator() {
@@ -28,7 +79,7 @@ describe('harmony', function() {
 				var deferred = Q.defer();
 				setTimeout(function() {
 					deferred.resolve('hello data');
-				}, 100);
+				}, 1);
 				return deferred.promise;
 			}
 
@@ -50,7 +101,7 @@ describe('harmony', function() {
 				var deferred = Q.defer();
 				setTimeout(function() {
 					deferred.reject(new Error('no data'));
-				}, 100);
+				}, 1);
 				return deferred.promise;
 			}			
 
