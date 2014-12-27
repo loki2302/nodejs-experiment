ad4e7ac038a41814cd07329b2f51125b5e64b0b2
loki2302 <loki2302@loki2302.me>
2014-12-27 16:22:18 +0300

    Added sequelize/promisesTests with harmony/yield/co

---
+++ sequelize-experiment/tests/promisesTests.js
@@ -0,0 +1,54 @@
+var Sequelize = require('sequelize');
+var co = require('co');
+
+exports.promisesTests = {
+	setUp: function(callback) {
+		var sequelize = new Sequelize('database', 'username', 'password', {
+			dialect: 'sqlite',
+			storage: 'my.db'
+		});
+		this.sequelize = sequelize;
+
+		this.Note = this.sequelize.define('Note', {
+			content: Sequelize.STRING
+		});
+
+		co(function* () {
+			yield sequelize.sync();
+		}).then(callback, function(e) {
+			throw e;
+		});
+	},
+
+	tearDown: function(callback) {
+		var sequelize = this.sequelize;
+		co(function* () {
+			yield sequelize.drop();
+		}).then(callback, function(e) {
+			throw e;
+		});
+	},
+
+	dummy: function(test) {
+		var sequelize = this.sequelize;
+		var Note = this.Note;
+
+		co(function* () {
+			var noteCount = yield Note.count();
+			test.equals(noteCount, 0);
+
+			var note = yield Note.create({ content: 'hi there' });
+			test.equals(note.id, 1);
+
+			noteCount = yield Note.count();
+			test.equals(noteCount, 1);
+
+			var allNotes = yield Note.all();
+			test.equals(allNotes.length, 1);
+			test.equals(allNotes[0].id, 1);
+			test.equals(allNotes[0].content, 'hi there');
+
+			test.done();
+		});
+	}
+};
