791cd4069d6c59551efbddc504718ea6f55b8568
loki2302 <loki2302@loki2302.me>
2014-12-27 14:37:16 +0300

    Added yield/q test

--- harmony-experiment/tests.js
+++ harmony-experiment/tests.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var Q = require('q');
 
 describe('harmony', function() {
 	describe('generator', function() {
@@ -20,6 +21,29 @@ describe('harmony', function() {
 			v = it.next();
 			assert.equal(v.value, undefined);
 			assert.equal(v.done, true);
+		});
+
+		it('should let me make promises look synchronous', function(done) {
+			function getData() {
+				var deferred = Q.defer();
+				setTimeout(function() {
+					deferred.resolve('hello data');
+				}, 100);
+				return deferred.promise;
+			}
+
+			function* controllerFunc() {
+				var data = yield getData();
+				assert.equal(data, 'hello data');
+				done();
+			}
+
+			var g = controllerFunc();
+			
+			var v = g.next();
+			v.value.then(function(result) {
+				g.next(result);
+			});
 		});
 	});	
 });
