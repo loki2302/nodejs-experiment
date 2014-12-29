709f22146a564bccc4d31e5b8fc046efbe25c8a2
loki2302 <loki2302@loki2302.me>
2014-12-29 23:02:47 +0300

    Added Sequelize transactions

--- koajs-experiment/app.js
+++ koajs-experiment/app.js
@@ -21,16 +21,47 @@ module.exports = function() {
     this.Note = Note;
     
     this.throwNoteNotFound = function() {
-      this.throw(404, 'Note not found');
+      this.status = 404;
+      this.body = "Note not found";
     };
 
     yield* next;
+  });
+
+  app.use(function* (next) {
+    var tx = yield sequelize.transaction({
+      isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED
+    });
+
+    this.tx = tx;
+    
+    try {
+      yield* next;
+
+      try {
+        yield tx.commit();
+        console.log("operation succeeded, commit succeeded");
+      } catch(commitException) {
+        console.log("operation succeeded, commit failed: %s", commitException);
+      }
+    } catch(operationException) {
+      try {
+        yield tx.rollback();
+        console.log("operation failed, rollback succeeded");
+      } catch(rollbackException) {
+        console.log("operation failed, rollback failed: %s", rollbackException);
+      }
+    }
   });
   
   app.use(router(app));
 
   app.param("note", function* (noteId, next) {
-    var note = yield this.Note.find(noteId);
+    var note = yield this.Note.find({ 
+      where: { id: noteId }
+    }, { 
+      transaction: this.tx
+    });
     if(!note) {
       this.throwNoteNotFound();
       return;
@@ -41,12 +72,16 @@ module.exports = function() {
   });
 
   app.get('/notes', function* (next) {
-    this.body = yield this.Note.findAll();
+    this.body = yield this.Note.findAll({
+      transaction: this.tx
+    });
   });
 
   app.post('/notes', function* (next) {   
     this.body = yield this.Note.create({
       content: this.request.body.content
+    }, {
+      transaction: this.tx
     });
   });
 
@@ -55,14 +90,18 @@ module.exports = function() {
   });
 
   app.delete('/notes/:note', function* (next) {
-    yield this.note.destroy();
+    yield this.note.destroy({
+      transaction: this.tx
+    });
 
     this.body = { 'message': 'ok' };
   });
 
   app.put('/notes/:note', function* (next) {
     this.note.content = this.request.body.content;
-    yield this.note.save();
+    yield this.note.save({
+      transaction: this.tx
+    });
 
     this.body = { 'message': 'ok' };
   });
