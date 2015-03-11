b637fd018a4e90eb292770a2a2d61218f034abfc
loki2302 <loki2302@loki2302.me>
2015-03-11 22:13:00 +0300

    Cleaning AppController tests

--- fe-test/app-controller.spec.js
+++ fe-test/app-controller.spec.js
@@ -1,64 +1,48 @@
 describe('AppController', function() {
-  var $controller;
-  var $rootScope;
+  var appController;
+  var $scope;
   var $location;
 
   beforeEach(module('app'));
 
-  beforeEach(inject(function(_$controller_, _$rootScope_, _$location_) {
-    $controller = _$controller_;
-    $rootScope = _$rootScope_;
+  beforeEach(inject(function($controller, $rootScope, _$location_) {
+    $scope = $rootScope;
     $location = _$location_;
+
+    appController = $controller('AppController', { 
+      $scope: $scope,  
+      $location: $location
+    });
   }));
 
   it('should say that "notes" is active when location is /notes', function() {    
-    var controller = $controller('AppController', { 
-      $scope: $rootScope,  
-      $location: $location
-    });
-
     $location.path('/notes');
-    $rootScope.$apply();
+    $scope.$apply();
 
-    expect($rootScope.isNavBarActive('notes')).toBe(true);
-    expect($rootScope.isNavBarActive('categories')).toBe(false);
+    expect($scope.isNavBarActive('notes')).toBe(true);
+    expect($scope.isNavBarActive('categories')).toBe(false);
   });
 
   it('should say that "categories" is active when location is /categories', function() {    
-    var controller = $controller('AppController', { 
-      $scope: $rootScope,  
-      $location: $location
-    });
-
     $location.path('/categories');
-    $rootScope.$apply();
+    $scope.$apply();
 
-    expect($rootScope.isNavBarActive('notes')).toBe(false);
-    expect($rootScope.isNavBarActive('categories')).toBe(true);
+    expect($scope.isNavBarActive('notes')).toBe(false);
+    expect($scope.isNavBarActive('categories')).toBe(true);
   });
 
   it('should say that no tab is active when location is unknown', function() {
-    var controller = $controller('AppController', { 
-      $scope: $rootScope,  
-      $location: $location
-    });
-
     $location.path('/google');
-    $rootScope.$apply();
+    $scope.$apply();
 
-    expect($rootScope.isNavBarActive('notes')).toBe(false);
-    expect($rootScope.isNavBarActive('categories')).toBe(false);
+    expect($scope.isNavBarActive('notes')).toBe(false);
+    expect($scope.isNavBarActive('categories')).toBe(false);
   });
 
   it('should say that tab is not activate when the tab is unknown', function() {
-    var controller = $controller('AppController', { 
-      $scope: $rootScope,  
-      $location: $location
-    });
-
     $location.path('/notes');
-    $rootScope.$apply();
+    $scope.$apply();
 
-    expect($rootScope.isNavBarActive('google')).toBe(false);
+    expect($scope.isNavBarActive('google')).toBe(false);
   });
 });
