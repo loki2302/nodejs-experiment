7f9d89541a8a7ef5f907865d142c18e5d3e628f8
loki2302 <loki2302@loki2302.me>
2015-03-11 10:18:43 +0300

    Protractor now uses Firefox when running on Travis and Chrome by default

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,15 @@
-exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['e2e-test/spec.js'],
-  capabilities: {
-    browserName: 'firefox'
-  },
-  baseUrl: 'http://localhost:3000/'
+var capabilities = {};
+if(process.env.TRAVIS) {
+  capabilities.browserName = 'firefox';
+} else {
+  capabilities.browserName = 'chrome';
+}
+
+module.exports = {
+  config: {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['e2e-test/spec.js'],
+    capabilities: capabilities,
+    baseUrl: 'http://localhost:3000/'
+  }
 };
