042c2c33e5b2bbe31d4ff9deb664616da38cb42d
loki2302 <loki2302@loki2302.me>
2015-03-12 09:58:31 +0300

    Added a positive "create note" test

--- fe-test/notes-controller.spec.js
+++ fe-test/notes-controller.spec.js
@@ -11,4 +11,48 @@ describe('NotesController', function() {
 
     expect($rootScope.notes).toBeDefined();
   }));
+
+  it('should be able to create a new note', inject(function($controller, $rootScope, $q) {
+    var createNoteResultDeferred = $q.defer();
+
+    var apiService = {
+      createNote: function(note) {
+        return createNoteResultDeferred.promise;
+      },
+      getNotes: function() {
+        return [{
+          id: 123,
+          content: 'hello there'
+        }];
+      }
+    };
+
+    var notesController = $controller('NotesController', {
+      $scope: $rootScope,
+      $q: $q,
+      notes: [],
+      apiService: apiService
+    });
+
+    spyOn(apiService, 'createNote').and.callThrough();
+    spyOn(apiService, 'getNotes').and.callThrough();
+
+    $rootScope.createNote({
+      content: 'hello there',
+      categories: []
+    });
+
+    expect(apiService.createNote).toHaveBeenCalled();
+    createNoteResultDeferred.resolve({
+      id: 123,
+      content: 'hello there'
+    });
+    
+    $rootScope.$apply();
+
+    expect(apiService.getNotes).toHaveBeenCalled();
+    $rootScope.$apply();
+
+    expect($rootScope.notes.length).toBe(1);
+  }));
 });
