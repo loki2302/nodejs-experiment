7070593ebea5830dcaebe819fbbf4eaa68541fc9
loki2302 <loki2302@loki2302.me>
2015-03-12 10:13:54 +0300

    Added an alternative implementation of positive "create note" test, based on $httpBackend mock

--- fe-test/notes-controller.spec.js
+++ fe-test/notes-controller.spec.js
@@ -12,7 +12,7 @@ describe('NotesController', function() {
     expect($rootScope.notes).toBeDefined();
   }));
 
-  it('should be able to create a new note', inject(function($controller, $rootScope, $q) {
+  it('should be able to create a new note [apiService mock]', inject(function($controller, $rootScope, $q) {
     var createNoteResultDeferred = $q.defer();
 
     var apiService = {
@@ -52,6 +52,39 @@ describe('NotesController', function() {
 
     expect(apiService.getNotes).toHaveBeenCalled();
     $rootScope.$apply();
+
+    expect($rootScope.notes.length).toBe(1);
+  }));
+
+  it('should be able to create a new note [$httpBackend mock]', inject(function($controller, $rootScope, $q, $httpBackend, apiService) {
+    var notesController = $controller('NotesController', {
+      $scope: $rootScope,
+      $q: $q,
+      notes: [],
+      apiService: apiService
+    });
+
+    $httpBackend.when('POST', '/api/notes')
+    .respond(201, {
+      id: 123,
+      content: 'hello there'
+    });
+
+    $httpBackend.when('GET', '/api/notes')
+    .respond(200, [{
+      id: 123,
+      content: 'hello there'
+    }]);
+
+    $rootScope.createNote({
+      content: 'hello there',
+      categories: []
+    });    
+
+    $httpBackend.flush();    
+
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
 
     expect($rootScope.notes.length).toBe(1);
   }));
