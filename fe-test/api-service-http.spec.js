9ea4c369671d2d17dea5b05568643aaf935d3649
loki2302 <loki2302@loki2302.me>
2015-03-14 20:21:01 +0300

    Reimplemented api2/createNote() tests using fluent interface

--- fe-test/api-service-http.spec.js
+++ fe-test/api-service-http.spec.js
@@ -1,81 +1,105 @@
 describe('api-service-http', function() {
   beforeEach(module('api2'));
 
+  function testCase(name) { 
+    return {
+      whenIMakeAnApiCall: function(apiCall) {
+        return {
+          itSendsTheRequest: function(requestDefinition) {
+            return {
+              andIfServerRespondsWith: function(responseDefinition) {
+                return {
+                  aCallSucceedsWith: function(resultDefinition) {
+                    it(name, inject(function($httpBackend, apiService2) {
+                      $httpBackend
+                        .expect(requestDefinition.method, requestDefinition.url)
+                        .respond(responseDefinition.status, responseDefinition.body);
+
+                      var onSuccess = jasmine.createSpy('onSuccess');
+                      apiCall(apiService2).then(onSuccess);
+
+                      $httpBackend.verifyNoOutstandingExpectation();
+
+                      $httpBackend.flush();
+                      $httpBackend.verifyNoOutstandingRequest();
+
+                      expect(onSuccess).toHaveBeenCalledWith(jasmine.objectContaining(resultDefinition));
+                    }));
+                  },
+                  aCallFailsWith: function(resultDefinition) {
+                    it(name, inject(function($httpBackend, apiService2) {
+                      $httpBackend
+                        .expect(requestDefinition.method, requestDefinition.url)
+                        .respond(responseDefinition.status, responseDefinition.body);
+
+                      var onError = jasmine.createSpy('onError');
+                      apiCall(apiService2).then(null, onError);
+
+                      $httpBackend.verifyNoOutstandingExpectation();
+
+                      $httpBackend.flush();
+                      $httpBackend.verifyNoOutstandingRequest();
+
+                      expect(onError).toHaveBeenCalledWith(jasmine.any(resultDefinition()));
+                    }));
+                  }
+                }
+              }              
+            }
+          }
+        }
+      }
+    }
+  };
+
   describe('createNote()', function() {
-    it('should return created note when 200', inject(function($httpBackend, apiService2) {
-      $httpBackend.expect('POST', '/api/notes').respond(200, {
-        id: 123,
-        text: 'hello',
-        categories: []
-      });
-
-      var onSuccess = jasmine.createSpy('onSuccess');
-      apiService2.createNote({
-        text: 'hello',
-        categories: []
-      }).then(onSuccess);
-
-      $httpBackend.verifyNoOutstandingExpectation();
-
-      $httpBackend.flush();
-      $httpBackend.verifyNoOutstandingRequest();
-
-      expect(onSuccess).toHaveBeenCalledWith(jasmine.objectContaining({
-        id: 123,
-        text: 'hello',
-        categories: []
-      }));
+    var errors;
+    beforeEach(inject(function(_errors_) {
+      errors = _errors_;
     }));
 
-    it('should throw connectivity error when 0', inject(function($httpBackend, apiService2, errors) {
-      $httpBackend.expect('POST', '/api/notes').respond(0, {});
+    testCase('should return created note when 200')
+      .whenIMakeAnApiCall(function(api) {
+        return api.createNote({
+          text: 'hello',
+          categories: []
+        });
+      })
+      .itSendsTheRequest({ method: 'POST', url: '/api/notes' })
+      .andIfServerRespondsWith({ status: 200, body: { id: 123, text: 'hello', categories: [] } })
+      .aCallSucceedsWith({ id: 123, text: 'hello', categories: [] });
 
-      var onError = jasmine.createSpy('onError');
-      apiService2.createNote({
-        text: 'hello',
-        categories: []
-      }).then(null, onError);
+    testCase('should throw connectivity error when 0')
+      .whenIMakeAnApiCall(function(api) {
+        return api.createNote({
+          text: 'hello',
+          categories: []
+        });
+      })
+      .itSendsTheRequest({ method: 'POST', url: '/api/notes' })
+      .andIfServerRespondsWith({ status: 0 })
+      .aCallFailsWith(function() { return errors.ConnectivityError; });
 
-      $httpBackend.verifyNoOutstandingExpectation();
+    testCase('should throw validation error when 400')
+      .whenIMakeAnApiCall(function(api) {
+        return api.createNote({
+          text: 'hello',
+          categories: []
+        });
+      })
+      .itSendsTheRequest({ method: 'POST', url: '/api/notes' })
+      .andIfServerRespondsWith({ status: 400 })
+      .aCallFailsWith(function() { return errors.ValidationError; });
 
-      $httpBackend.flush();
-      $httpBackend.verifyNoOutstandingRequest();
-
-      expect(onError).toHaveBeenCalledWith(jasmine.any(errors.ConnectivityError));
-    }));
-
-    it('should throw validation error when 400', inject(function($httpBackend, apiService2, errors) {
-      $httpBackend.expect('POST', '/api/notes').respond(400, {});
-
-      var onError = jasmine.createSpy('onError');
-      apiService2.createNote({
-        text: 'hello',
-        categories: []
-      }).then(null, onError);
-
-      $httpBackend.verifyNoOutstandingExpectation();
-
-      $httpBackend.flush();
-      $httpBackend.verifyNoOutstandingRequest();
-
-      expect(onError).toHaveBeenCalledWith(jasmine.any(errors.ValidationError));
-    }));
-
-    it('should throw unexpected error when 500', inject(function($httpBackend, apiService2, errors) {
-      $httpBackend.expect('POST', '/api/notes').respond(500, {});
-
-      var onError = jasmine.createSpy('onError');
-      apiService2.createNote({
-        text: 'hello',
-        categories: []
-      }).then(null, onError);
-
-      $httpBackend.verifyNoOutstandingExpectation();
-
-      $httpBackend.flush();
-      $httpBackend.verifyNoOutstandingRequest();
-
-      expect(onError).toHaveBeenCalledWith(jasmine.any(errors.UnexpectedError));
-    }));
+    testCase('should throw unexpected error when 500')
+      .whenIMakeAnApiCall(function(api) {
+        return api.createNote({
+          text: 'hello',
+          categories: []
+        });
+      })
+      .itSendsTheRequest({ method: 'POST', url: '/api/notes' })
+      .andIfServerRespondsWith({ status: 500 })
+      .aCallFailsWith(function() { return errors.UnexpectedError; });      
   });  
 });
