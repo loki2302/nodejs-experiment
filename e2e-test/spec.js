6cac3ae693c4cc52ab6dde74f23bddccd1af4afa
loki2302 <loki2302@loki2302.me>
2015-03-15 13:08:19 +0300

    Added more e2e tests

--- e2e-test/spec.js
+++ e2e-test/spec.js
@@ -2,9 +2,16 @@ var enableDestroy = require('server-destroy');
 var makeModels = require("../be-src/models.js");
 var makeApp = require("../be-src/app.js");
 
-var NotesPage = function() {
+var AppNavbar = function() {
+  this.notesItem = element(by.css('.nav-notes'));
+  this.categoriesItem = element(by.css('.nav-categories'));
+};
+
+var App = function() {
   this.getTitle = function() { return browser.getTitle(); };
-  
+};
+
+var NotesPage = function() { 
   this.noNotesAlert = element(by.css('#no-notes-alert'));
   
   this.noteList = element(by.css('#note-list'));
@@ -14,6 +21,10 @@ var NotesPage = function() {
   this.newNoteEditor = element(by.css('#new-note-editor'));
   this.newNoteEditorContent = element(by.css('#new-note-editor #content'));
   this.newNoteEditorSubmit = element(by.css('#new-note-editor button'));  
+};
+
+var CategoriesPage = function() {
+  this.noCategoriesAlert = element(by.css('#no-categories-alert'));
 };
 
 describe('app', function() {
@@ -41,16 +52,61 @@ describe('app', function() {
     });
   });
 
-  describe('by default', function() {
-    var notesPage;
+  describe('App', function() {
+    describe('defaults', function() {
+      var appPage;
+      beforeEach(function() {
+        browser.get('/');
+        app = new App();
+      });
 
-    beforeEach(function() {
-      browser.get('/');
-      notesPage = new NotesPage();
+      it('should have a title set to nodejs-app-experiment', function() {
+        expect(app.getTitle()).toBe('nodejs-app-experiment');
+      });
+
+      it('should open Notes page by default', function() {
+        // WTF '/notes' is an absolute URL?
+        expect(browser.getLocationAbsUrl()).toBe('/notes');
+      });
     });
 
-    it('should have page title set to nodejs-app-experiment', function() {
-      expect(notesPage.getTitle()).toBe('nodejs-app-experiment');
+    describe('navbar', function() {
+      var appNavbar;
+      beforeEach(function() {
+        appNavbar = new AppNavbar()
+      });
+
+      it('should hightlight Notes when Notes page is active', function() {
+        browser.get('/notes');
+        expect(appNavbar.notesItem.getAttribute('class')).toContain('active');
+        expect(appNavbar.categoriesItem.getAttribute('class')).not.toContain('active');
+      });
+
+      it('should hightlight Categories when Categories page is active', function() {
+        browser.get('/categories');
+        expect(appNavbar.notesItem.getAttribute('class')).not.toContain('active');
+        expect(appNavbar.categoriesItem.getAttribute('class')).toContain('active');
+      });
+
+      it('should switch from notes to categories, when notes page is active and one clicks categories', function() {
+        browser.get('/notes');
+        appNavbar.categoriesItem.click();
+        expect(browser.getLocationAbsUrl()).toBe('/categories');
+      });
+
+      it('should switch from categories to notes, when categories page is active and one clicks notes', function() {
+        browser.get('/categories');
+        appNavbar.notesItem.click();
+        expect(browser.getLocationAbsUrl()).toBe('/notes');
+      });
+    });    
+  });
+
+  describe('Notes page', function() {
+    var notesPage;
+    beforeEach(function() {
+      browser.get('/notes');
+      notesPage = new NotesPage();
     });
 
     it('should display "there are no notes" message', function() {
@@ -79,6 +135,19 @@ describe('app', function() {
       expect(notesPage.noteListNoteIdByNoteIndex(0).getText()).toBe('1');
       expect(notesPage.noteListNoteContentByNoteIndex(0).isPresent()).toBe(true);
       expect(notesPage.noteListNoteContentByNoteIndex(0).getText()).toBe('hello there');
+    });
+  });
+
+  describe('Categories page', function() {
+    var categoriesPage;
+    beforeEach(function() {
+      browser.get('/categories');
+      categoriesPage = new CategoriesPage();
+    });
+
+    it('should display "there are no categories" message', function() {
+      expect(categoriesPage.noCategoriesAlert.isPresent()).toBe(true);
+      expect(categoriesPage.noCategoriesAlert.getText()).toBe('There are no categories so far.');
     });
   });
 });
