381ab50f53c56d1f24f8c4aec736999c9235769c
loki2302 <loki2302@loki2302.me>
2015-03-15 12:07:10 +0300

    Added more e2e tests

--- e2e-test/spec.js
+++ e2e-test/spec.js
@@ -27,13 +27,41 @@ describe('app', function() {
     });
   });
 
-  it('should work #1', function() {
-    browser.get('/');
-    expect(browser.getTitle()).toEqual('nodejs-app-experiment');
-  });
+  describe('by default', function() {
+    beforeEach(function() {
+      browser.get('/');
+    });
 
-  it('should work #2', function() {
-    browser.get('/');
-    expect(browser.getTitle()).toEqual('nodejs-app-experiment');
+    it('should have page title set to nodejs-app-experiment', function() {      
+      expect(browser.getTitle()).toEqual('nodejs-app-experiment');
+    });
+
+    it('should display "there are no notes" message', function() {
+      expect(element(by.css('#no-notes-alert')).isPresent()).toBe(true);
+      expect(element(by.css('#no-notes-alert')).getText()).toEqual('There are no notes so far.');
+    });
+
+    it('should not display a list of notes', function() {
+      expect(element(by.css('#note-list')).isPresent()).toBe(false);
+    });
+
+    it('should have a new note editor', function() {
+      expect(element(by.css('#new-note-editor')).isPresent()).toBe(true);
+    });
+
+    it('should let me create a note', function() {
+      expect(element(by.css('#new-note-editor #content')).isPresent()).toBe(true);
+      element(by.css('#new-note-editor #content')).sendKeys('hello there');
+
+      expect(element(by.css('#new-note-editor button')).isPresent()).toBe(true);
+      element(by.css('#new-note-editor button')).click();
+
+      expect(element(by.css('#no-notes-alert')).isPresent()).toBe(false);
+      expect(element(by.css('#note-list')).isPresent()).toBe(true);
+      expect(element(by.css('#note-list li .note-id')).isPresent()).toBe(true);
+      expect(element(by.css('#note-list li .note-id')).getText()).toBe('1');
+      expect(element(by.css('#note-list li .note-content')).isPresent()).toBe(true);
+      expect(element(by.css('#note-list li .note-content')).getText()).toBe('hello there');
+    });
   });
 });
