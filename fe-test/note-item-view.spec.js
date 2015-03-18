9874646e0cf9de87a3c0352ea6c685184ec405de
loki2302 <loki2302@loki2302.me>
2015-03-18 11:28:25 +0300

    Added 'should delete' test to NoteItemView spec

--- fe-test/note-item-view.spec.js
+++ fe-test/note-item-view.spec.js
@@ -6,12 +6,44 @@ describe('NoteItemView directive', function() {
   var element;
   beforeEach(inject(function($rootScope, $compile) {
     $scope = $rootScope;
-    element = $compile('<note-item-view></note-item-view>')($scope);
-    $scope.$digest();
+    element = $compile('<note-item-view note="n" delete="onDeleteNote(note)"></note-item-view>')($scope);
   }));
 
-  it('should do at least something', inject(function() {
+  it('should somehow be set up', inject(function() {
+    $scope.$apply(function() {
+      $scope.n = {
+        id: 123,
+        content: 'hello there',
+        categories: []
+      };
+    });
+    
+    expect(element.find('.note-id').text()).toBe('123');
+    expect(element.find('.note-content').text()).toBe('hello there');
     expect(element.text()).toContain('Delete');
     expect(element.text()).toContain('Edit');
+    expect(element.text()).toContain('Linked to no categories');
+  }));
+
+  it('should delete', inject(function($q) {
+    var deleteNoteDeferred = $q.defer();
+    var onDeleteNote = jasmine.createSpy('onDeleteNote').and.returnValue(deleteNoteDeferred.promise);
+    $scope.$apply(function() {
+      $scope.n = {
+        id: 123,
+        content: 'hello there',
+        categories: []
+      };
+
+      $scope.onDeleteNote = onDeleteNote;
+    });
+
+    expect(element.find('.delete').hasClass('disabled')).toBe(false);
+    element.find('.delete').click();
+    expect(onDeleteNote).toHaveBeenCalledWith(jasmine.objectContaining({
+      id: 123
+    }));
+
+    expect(element.find('.delete').hasClass('disabled')).toBe(true);
   }));
 });
