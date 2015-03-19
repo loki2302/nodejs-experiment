7b34b88f963c4b3da12da3dca1efd0d69801c3da
loki2302 <loki2302@loki2302.me>
2015-03-19 11:40:04 +0300

    Added WTFs to playground

--- fe-test/playground.spec.js
+++ fe-test/playground.spec.js
@@ -1,29 +1,140 @@
 describe('playground', function() {
-  beforeEach(module(function($compileProvider) {
-    $compileProvider.directive('testNoteView', function() {
-      return {
-        restrict: 'E',
-        require: 'ngModel',
-        scope: {
-          note: "=ngModel",
-          edit: "&"
-        },
-        template: 
-        '<div>' + 
-        '  <span class="note-text">{{note.text}}</span>' + 
-        '  <button class="note-edit" type="button" ng-click="edit({note: note})">Edit</button>' + 
-        '</div>',
-        controller: function($scope, $element, $attrs, $transclude, /*any injectables*/ $log) {
-          // console.log('controller'); // 1: before link
-        },
-        link: function(scope, element, attrs /*, parentController */) {
-          // console.log('link'); // 2: after controller
-        }        
+  describe("testNoteEdit directive", function() {
+    beforeEach(module(function($compileProvider) {
+      $compileProvider
+      .directive('testNoteEdit', function() {
+        return {
+          restrict: 'E',
+          require: 'ngModel',
+          scope: {
+            submit: '&',
+            note: '=ngModel'
+          },
+          template: 
+          '<form ng-submit="handleSubmit()">' +
+          '<input type="text" class="note-text" ng-model="note.text">' +
+          '<button type="submit" class="submit">Submit</button>' +
+          '</form>',
+          link: function(scope) {
+            // do setAllFieldsValid
+            scope.handleSubmit = function() {
+              //scope.vf.setAllFieldsValid();
+              console.log("SUBMIT", navigator.userAgent);
+
+              scope.submit({note: scope.note}).then(function(resolution) {
+                // success, resolution is ignored
+                console.log('success', resolution, navigator.userAgent);
+              }, function(error) {
+                // error, resoltion is expected to be a validation error map
+                console.log('error', error);
+                //scope.vf.setFieldErrors(error);
+              });
+            };
+          }
+        };
+      })
+      /*.directive('validationFacade', function() {
+        // use like this: <form be="be">
+        // this will publish 'be' with setAllFieldsValid and setFieldErrors
+        // to the related scope
+        return {
+          require: 'form',
+          link: function(scope, element, attrs, ctrl) {
+            var errors = {};
+            scope[attrs.validationFacade] = {
+              setAllFieldsValid: function() {
+                angular.forEach(ctrl, function(formElement, fieldName) {
+                  if(fieldName[0] === '$') {
+                    return;
+                  }
+
+                  ctrl[fieldName].$setValidity('omg', true);              
+                });
+                errors = {};
+              },
+              setFieldErrors: function(errorMap) {
+                angular.forEach(errorMap, function(message, fieldName) {
+                  ctrl[fieldName].$setValidity('omg', false);
+                  ctrl[fieldName].$setPristine();
+                });
+                errors = errorMap;
+              },
+              getFieldError: function(fieldName) {
+                return errors[fieldName];
+              },
+              isError: function(fieldName) {
+                return ctrl[fieldName].$invalid && ctrl[fieldName].$pristine;
+              }
+            };
+          }
+        };
+      });*/
+    }));
+
+    var $rootScope;
+    var $scope;    
+    beforeEach(inject(function(_$rootScope_) {
+      $rootScope = _$rootScope_;
+      $scope = $rootScope.$new();
+    }));
+
+    it('positive case should work', inject(function($compile, $q) {      
+      var element = $compile('<test-note-edit ng-model="note" submit="onSubmitClicked(note)"></test-note-edit>')($scope);
+      $scope.note = {
+        text: 'hello'
       };
-    });
-  }));
+      $scope.onSubmitClicked = jasmine.createSpy('onSubmitClicked').and.returnValue($q.when(123));
+      $rootScope.$digest();
+
+      element.find('.submit').submit(); // WTF??? works OK in FF and Chrome
+      // element.find('button[type="submit"]').click(); // WTF??? only triggers in Chrome
+      // element.find('.submit').click(); // WTF??? only triggers in Chrome
+      // element.find('button').click(); // WTF??? only triggers in Chrome
+
+      console.log(element.find('button'));
+      
+      expect($scope.onSubmitClicked).toHaveBeenCalled();
+    }));
+
+    /*it('should work', inject(function($compile, $q) {
+      var element = $compile('<test-note-edit ng-model="note" submit="onSubmitClicked(note)"></test-note-edit>')($scope);
+      $scope.onSubmitClicked = function(m) {
+        console.log('onSubmitClicked', m);
+        return $q.when();
+      };
+
+      $rootScope.$digest();
+      //expect(element.find('div').text()).toBe('hello');
+      element.find('button').click();
+    }));*/
+  });
+  
 
   describe("testNoteView directive", function() {
+    beforeEach(module(function($compileProvider) {
+      $compileProvider.directive('testNoteView', function() {
+        return {
+          restrict: 'E',
+          require: 'ngModel',
+          scope: {
+            note: "=ngModel",
+            edit: "&"
+          },
+          template: 
+          '<div>' + 
+          '  <span class="note-text">{{note.text}}</span>' + 
+          '  <button class="note-edit" type="button" ng-click="edit({note: note})">Edit</button>' + 
+          '</div>',
+          controller: function($scope, $element, $attrs, $transclude, /*any injectables*/ $log) {
+            // console.log('controller'); // 1: before link
+          },
+          link: function(scope, element, attrs /*, parentController */) {
+            // console.log('link'); // 2: after controller
+          }        
+        };
+      });
+    }));
+
     var $rootScope;
     var $scope;    
     beforeEach(inject(function(_$rootScope_) {
