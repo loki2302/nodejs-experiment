4becf12947167dfe9b2898bd4abadde820b8438c
loki2302 <loki2302@loki2302.me>
2015-03-19 09:02:11 +0300

    Cleaning up playground

--- fe-test/playground.spec.js
+++ fe-test/playground.spec.js
@@ -29,19 +29,7 @@ describe('playground', function() {
     beforeEach(inject(function(_$rootScope_) {
       $rootScope = _$rootScope_;
       $scope = $rootScope.$new();
-    }));
-
-    // TODO
-    it('should pass the clicking test', inject(function($compile) {
-      element = $compile('<test-note-view ng-model="myNote" edit="onEdit(note)"></test-note-view>')($scope);
-      $scope.myNote = {
-        text: 'hello there',        
-      };
-      $scope.onEdit = jasmine.createSpy('onEdit');
-      $rootScope.$digest();
-      element.find('.note-edit').click();
-      expect($scope.onEdit).toHaveBeenCalledWith($scope.myNote);
-    }));
+    }));    
 
     describe('when there is no ngModel', function() {
       it('should not compile', inject(function($compile) {        
@@ -57,30 +45,47 @@ describe('playground', function() {
         element = $compile('<test-note-view ng-model="myNote"></test-note-view>')($scope);
       }));
 
-      it('should have a note-text element', inject(function() {
+      it('should have a note-text element', function() {
         expect(element.find('.note-text')).toBeDefined();
-      }));
+      });
 
-      it('should have a note-edit element', inject(function() {
+      it('should have a note-edit element', function() {
         expect(element.find('.note-edit')).toBeDefined();
-      }));
+      });
 
       describe('and ngModel references an undefined', function() {
-        it('should display an empty text', inject(function() {
+        it('should display an empty text', function() {
           $rootScope.$digest();
           expect(element.find('.note-text').text()).toBe('');
-        }));
+        });
       });
 
       describe('and ngModel references an object', function() {
-        it('should display a text', inject(function() {
+        it('should display a text', function() {
           $scope.myNote = {
             text: 'hello there'
           };
           $rootScope.$digest();
           expect(element.find('.note-text').text()).toBe('hello there');
-        }));
+        });
       });
     });
+
+    it('should call an edit handler when handler is set and button gets clicked', inject(function($compile) {
+      element = $compile('<test-note-view ng-model="myNote" edit="onEdit(note)"></test-note-view>')($scope);
+      $scope.myNote = {
+        text: 'hello there',        
+      };
+      $scope.onEdit = jasmine.createSpy('onEdit');
+      $rootScope.$digest();
+      element.find('.note-edit').click();
+      expect($scope.onEdit).toHaveBeenCalledWith($scope.myNote);
+    }));
+
+    it('should do nothing when handler is not set and button gets clicked', inject(function($compile) {
+      element = $compile('<test-note-view ng-model="myNote"></test-note-view>')($scope);
+      $rootScope.$digest();
+      element.find('.note-edit').click();
+    }));
   });
 });
