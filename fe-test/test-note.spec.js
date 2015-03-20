5a9fa63bbd3a0c5bda5aa51a55026c10729a79f6
loki2302 <loki2302@loki2302.me>
2015-03-20 08:50:10 +0300

    Implemented basic Edit/Cancel behavior for testNoteInlineEdit

--- fe-test/test-note.spec.js
+++ fe-test/test-note.spec.js
@@ -5,14 +5,51 @@ describe('directives.testNote', function() {
     beforeEach(module(function($compileProvider) {
       $compileProvider.directive('testNoteInlineEdit', function() {
         return {
-          template: '<div>hello</div>'
+          restrict: 'E',
+          template: 
+          '<div ng-if="isEditMode">' + 
+          '  editing <button type="button" class="cancel-edit-button" ng-click="onCancelEditClicked()">Cancel</button>' + 
+          '</div>' +
+          '<div ng-if="!isEditMode">' + 
+          '  viewing <button type="button" class="edit-button" ng-click="onEditClicked()">Edit</button>' + 
+          '</div>',
+          link: function(scope) {
+            scope.isEditMode = false;
+
+            scope.onEditClicked = function() {
+              scope.isEditMode = true;
+            };
+
+            scope.onCancelEditClicked = function() {
+              scope.isEditMode = false;
+            };
+          }
         };
       });
     }));
 
-    it('should even exist', inject(function($compile, $rootScope) {
+    it('should be in view mode by default', inject(function($compile, $rootScope) {
       var element = $compile('<test-note-inline-edit></test-note-inline-edit>')($rootScope);
-      expect(element.text()).toBe('hello');
+      $rootScope.$digest();
+      expect(element.find('button.edit-button').length).toBe(1);
+      expect(element.find('button.cancel-edit-button').length).toBe(0);
+    }));
+
+    it('should switch to edit mode when Edit button get clicked', inject(function($compile, $rootScope) {
+      var element = $compile('<test-note-inline-edit></test-note-inline-edit>')($rootScope);
+      $rootScope.$digest();
+      element.find('button.edit-button').click();
+      expect(element.find('button.edit-button').length).toBe(0);
+      expect(element.find('button.cancel-edit-button').length).toBe(1);
+    }));
+
+    it('should switch back to view mode when Cancel button gets clicked', inject(function($compile, $rootScope) {
+      var element = $compile('<test-note-inline-edit></test-note-inline-edit>')($rootScope);
+      $rootScope.$digest();
+      element.find('button.edit-button').click();
+      element.find('button.cancel-edit-button').click();
+      expect(element.find('button.edit-button').length).toBe(1);
+      expect(element.find('button.cancel-edit-button').length).toBe(0);
     }));
   });
 
