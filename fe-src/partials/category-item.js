6d6e6581c755625a4c841b421d4666a37284ed92
loki2302 <loki2302@loki2302.me>
2015-03-22 18:19:41 +0300

    Extracted categoryItem template

--- fe-src/partials/category-item.js
+++ fe-src/partials/category-item.js
@@ -10,28 +10,7 @@ angular.module('directives.categories.categoryItem', [
       onDelete: '&',
       onUpdate: '&'
     },
-    template:
-    '<div ng-if="!editingCategory">' +
-    '  <span class="badge category-id">{{category.id}}</span>' +
-    '  <span class="category-name">{{category.name}}</span>' +
-    '  <button ng-click="deleteCategory()" class="btn btn-default" ng-class="{' + "'disabled': busy" + '}">Delete</button>' +
-    '  <button ng-click="switchToEditMode()" class="btn btn-default" ng-class="{' + "'disabled': busy" + '}">Edit</button>' +
-    '</div>' +
-    '<div ng-if="editingCategory">' +
-    '  <form ng-submit="updateCategory(vf)" validation-facade="vf">' +
-    '    <fieldset ng-disabled="busy">' +
-    '      <div class="form-group" ng-class="{' + "'has-error'" + ':vf.isError(' + "'name'" + ')}">' +
-    '        <label for="name" class="control-label">Name</label>' +
-    '        <input type="text" class="form-control" id="name" name="name" ng-model="editingCategory.name">' +
-    '        <p class="help-block" ng-if="vf.isError(' + "'name'" + ')">{{vf.getFieldError(' + "'name'" + ')}}</p>' +
-    '      </div>' +
-    '      <div class="form-group">' +
-    '        <button type="submit" class="btn btn-default">Update</button>' +
-    '        <button type="button" class="btn btn-default" ng-click="switchToViewMode()">Cancel</button>' +
-    '      </div>' +
-    '    </fieldset>' +
-    '  </form>' +
-    '</div>',
+    templateUrl: 'partials/category-item.html',
     link: function(scope) {
       scope.editingCategory = null;
 
