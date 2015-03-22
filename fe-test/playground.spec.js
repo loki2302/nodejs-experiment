185f3740e9d1ce01944773d6aa28ed97e3351d75
loki2302 <loki2302@loki2302.me>
2015-03-22 09:13:14 +0300

    Cleaned up test-note.spec.js

--- fe-test/playground.spec.js
+++ fe-test/playground.spec.js
@@ -1,2 +1,18 @@
-describe('playground', function() {  
+describe('playground', function() {
+  describe('inline editor', function() {
+    beforeEach(module(function($compileProvider) {
+      $compileProvider.directive('noteItem', function() {
+        return {
+          restrict: 'E',
+          template: '<div>hello</div>'
+        }
+      });
+    }));
+
+    it('should work', inject(function($compile, $rootScope) {
+      var element = $compile('<note-item></note-item>')($rootScope);
+      $rootScope.$digest();
+      expect(element.text()).toBe('hello');
+    }));
+  });
 });
