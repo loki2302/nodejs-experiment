792b29b21dcf9b7349544c484cf8c094f35172b5
loki2302 <loki2302@loki2302.me>
2015-03-22 13:06:26 +0300

    createCategory now has on-create

--- fe-src/partials/create-category.js
+++ fe-src/partials/create-category.js
@@ -3,12 +3,12 @@ angular.module('directives.categories.createCategory', ['app.directives.utils'])
   return {
     restrict: 'E',
     scope: {
-      create: '&',
+      onCreate: '&',
       busy: '='
     },
     template:
     '<h3>Create category</h3>' +
-    '<form ng-submit="onSubmit()" validation-facade="vf">' + 
+    '<form ng-submit="createCategory()" validation-facade="vf">' + 
     '  <fieldset ng-disabled="busy">' +
     '    <div class="form-group" ng-class="{' + "'has-error'" + ':vf.isError(' + "'name'" + ')}">' +
     '      <label for="name" class="control-label">Name</label>' +
@@ -23,9 +23,9 @@ angular.module('directives.categories.createCategory', ['app.directives.utils'])
     link: function(scope) {
       scope.category = makeCategoryTemplate();
 
-      scope.onSubmit = function() {
+      scope.createCategory = function() {
         scope.vf.setAllFieldsValid();
-        scope.create({
+        scope.onCreate({
           category: scope.category
         }).then(function() {
           scope.category = makeCategoryTemplate();
@@ -38,69 +38,6 @@ angular.module('directives.categories.createCategory', ['app.directives.utils'])
         return {
           name: ''
         };
-      };
-    }
-  };
-});
-
-angular.module('directives.categories.categoryItem', ['app.directives.utils'])
-.directive('categoryItem', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      category: '=',
-      busy: '=',
-      onDelete: '&',
-      onUpdate: '&'
-    },
-    template:
-    '<div ng-if="!editingCategory">' +
-    '  <span class="badge category-id">{{category.id}}</span>' +
-    '  <span class="category-name">{{category.name}}</span>' +
-    '  <button ng-click="deleteCategory()" class="btn btn-default" ng-class="{' + "'disabled': busy" + '}">Delete</button>' +
-    '  <button ng-click="switchToEditMode()" class="btn btn-default" ng-class="{' + "'disabled': busy" + '}">Edit</button>' +
-    '</div>' +
-    '<div ng-if="editingCategory">' +
-    '  <form ng-submit="updateCategory(vf)" validation-facade="vf">' +
-    '    <fieldset ng-disabled="busy">' +
-    '      <div class="form-group" ng-class="{' + "'has-error'" + ':vf.isError(' + "'name'" + ')}">' +
-    '        <label for="name" class="control-label">Name</label>' +
-    '        <input type="text" class="form-control" id="name" name="name" ng-model="editingCategory.name">' +
-    '        <p class="help-block" ng-if="vf.isError(' + "'name'" + ')">{{vf.getFieldError(' + "'name'" + ')}}</p>' +
-    '      </div>' +
-    '      <div class="form-group">' +
-    '        <button type="submit" class="btn btn-default">Update</button>' +
-    '        <button type="button" class="btn btn-default" ng-click="switchToViewMode()">Cancel</button>' +
-    '      </div>' +
-    '    </fieldset>' +
-    '  </form>' +
-    '</div>',
-    link: function(scope) {
-      scope.editingCategory = null;
-
-      scope.deleteCategory = function() {
-        scope.onDelete({
-          category: scope.category
-        });
-      };
-
-      scope.switchToEditMode = function() {        
-        scope.editingCategory = angular.copy(scope.category);
-      };
-
-      scope.switchToViewMode = function() {
-        scope.editingCategory = null;
-      };
-
-      scope.updateCategory = function(validationFacade) {        
-        validationFacade.setAllFieldsValid();
-        scope.onUpdate({
-          updatedCategory: scope.editingCategory
-        }).then(function() {
-          scope.switchToViewMode();
-        }, function(error) {          
-          validationFacade.setFieldErrors(error);
-        });
       };
     }
   };
