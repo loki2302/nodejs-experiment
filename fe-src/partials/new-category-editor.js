c9e5d76ace82f7e09fce607172997b67c50d7070
loki2302 <loki2302@loki2302.me>
2015-03-22 18:23:57 +0300

    Extracted newCategoryEditor template

--- fe-src/partials/new-category-editor.js
+++ fe-src/partials/new-category-editor.js
@@ -8,20 +8,7 @@ angular.module('directives.categories.newCategoryEditor', [
       onCreate: '&',
       busy: '='
     },
-    template:
-    '<h3>Create category</h3>' +
-    '<form ng-submit="createCategory()" validation-facade="vf">' + 
-    '  <fieldset ng-disabled="busy">' +
-    '    <div class="form-group" ng-class="{' + "'has-error'" + ':vf.isError(' + "'name'" + ')}">' +
-    '      <label for="name" class="control-label">Name</label>' +
-    '      <input type="text" class="form-control" id="name" name="name" ng-model="category.name">' +
-    '      <p class="help-block" ng-if="vf.isError(' + "'name'" + ')">{{vf.getFieldError(' + "'name'" + ')}}</p>' +
-    '    </div>' +
-    '    <div class="form-group">' +
-    '      <button type="submit" class="btn btn-default">Create</button>' +
-    '    </div>' +
-    '  </fieldset>' +
-    '</form>',
+    templateUrl: 'partials/new-category-editor.html',
     link: function(scope) {
       scope.category = makeCategoryTemplate();
 
