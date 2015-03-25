fbc61808dcbd71fd9bf615fd013eb207fc537393
loki2302 <loki2302@loki2302.me>
2015-03-25 09:47:31 +0300

    Now using co-mocha to get rid of explicit co() calls everywhere

--- sequelize-experiment/tests/manyToManyThroughTests.spec.js
+++ sequelize-experiment/tests/manyToManyThroughTests.spec.js
@@ -1,14 +1,15 @@
+require('co-mocha');
+
 var expect = require('chai').expect;
 
 var Sequelize = require('sequelize');
-var co = require('co');
 
 describe('Many to many through', function() {
   var sequelize;
   var Book;
   var Author;
   var BooksAuthors;
-  beforeEach(function(done) {
+  beforeEach(function* () {
     sequelize = new Sequelize('sqlite://my.db');
 
     Book = sequelize.define('Book', {
@@ -26,56 +27,44 @@ describe('Many to many through', function() {
     Book.hasMany(Author, { through: BooksAuthors });
     Author.hasMany(Book, { through: BooksAuthors });
 
-    co(function* () {
-      yield sequelize.sync();
-    }).then(done, done);
+    yield sequelize.sync();
   });
 
   describe('omg', function() {
     var anotherFineMythBook;
     var robertAsprinPerson;
-    beforeEach(function(done) {
-      co(function* () {
-        anotherFineMythBook = yield Book.create({
-          title: 'Another Fine Myth'
-        });
+    beforeEach(function* () {
+      anotherFineMythBook = yield Book.create({
+        title: 'Another Fine Myth'
+      });
 
-        robertAsprinPerson = yield Author.create({
-          name: 'Robert Asprin'
-        });
-      }).then(done, done);
+      robertAsprinPerson = yield Author.create({
+        name: 'Robert Asprin'
+      });
     });
 
-    it('should let me add author to book', function(done) {
-      co(function* () {
-        yield anotherFineMythBook.addAuthor(robertAsprinPerson, {
-          isCorrect: true
-        });
-      }).then(done, done);
+    it('should let me add author to book', function* () {
+      yield anotherFineMythBook.addAuthor(robertAsprinPerson, {
+        isCorrect: true
+      });
     });
 
-    it('should let me add book to author', function(done) {
-      co(function* () {
-        yield robertAsprinPerson.addBook(anotherFineMythBook, {
-          isCorrect: true
-        });
-      }).then(done, done);
+    it('should let me add book to author', function* () {
+      yield robertAsprinPerson.addBook(anotherFineMythBook, {
+        isCorrect: true
+      });
     });
 
-    afterEach(function(done) {
-      co(function* () {
-        var authors = yield anotherFineMythBook.getAuthors();
-        expect(authors.length).to.equal(1);
+    afterEach(function* () {
+      var authors = yield anotherFineMythBook.getAuthors();
+      expect(authors.length).to.equal(1);
 
-        var books = yield robertAsprinPerson.getBooks();
-        expect(books.length).to.equal(1);
-      }).then(done, done);
+      var books = yield robertAsprinPerson.getBooks();
+      expect(books.length).to.equal(1);
     });
   });
 
-  afterEach(function(done) {
-    co(function* () {
-      yield sequelize.drop();
-    }).then(done, done);
+  afterEach(function* () {
+    yield sequelize.drop();
   });
 });
