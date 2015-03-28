9187bfeae4f9bae632a61a2908c8b2d4d68e6063
loki2302 <loki2302@loki2302.me>
2015-03-28 19:06:11 +0300

    Added connection/entity removal tests

--- sequelize-experiment/tests/manyToManyThroughTests.spec.js
+++ sequelize-experiment/tests/manyToManyThroughTests.spec.js
@@ -34,7 +34,7 @@ describe('Sequelize many-to-many-through', function() {
     yield sequelize.drop();
   });
 
-  describe('Connecting entities from both ends', function() {
+  describe('Given that there is one project and one employee', function() {
     var aProject;
     var anEmployee;
     beforeEach(function* () {
@@ -42,24 +42,68 @@ describe('Sequelize many-to-many-through', function() {
       anEmployee = yield Employee.create({ name: 'employee 1' });
     });
 
-    it('should let me add employee to project', function* () {
-      yield aProject.addParticipant(anEmployee, { role: 'developer' });
+    describe('and they are not connected', function() {
+      it('should let me add an employee to project as a participant', function* () {
+        yield aProject.addParticipant(anEmployee, { role: 'developer' });
+      });
+
+      it('should let me add a project to employee as a participation', function* () {
+        yield anEmployee.addParticipation(aProject, { role: 'developer' });
+      });
+
+      afterEach(function* () {
+        var projectParticipants = yield aProject.getParticipants();
+        expect(projectParticipants.length).to.equal(1);
+        expect(projectParticipants[0].name).to.equal('employee 1');
+        expect(projectParticipants[0].Participation.role).to.equal('developer');
+
+        var employeeParticipations = yield anEmployee.getParticipations();
+        expect(employeeParticipations.length).to.equal(1);
+        expect(employeeParticipations[0].name).to.equal('project 1');
+        expect(employeeParticipations[0].Participation.role).to.equal('developer');
+      });
     });
 
-    it('should let me add a project to employee', function* () {
-      yield anEmployee.addParticipation(aProject, { role: 'developer' });
-    });
+    describe('and they are connected', function() {
+      beforeEach(function* () {
+        yield aProject.addParticipant(anEmployee, { role: 'developer' });
+      });
 
-    afterEach(function* () {
-      var projectEmployees = yield aProject.getParticipants();
-      expect(projectEmployees.length).to.equal(1);
-      expect(projectEmployees[0].name).to.equal('employee 1');
-      expect(projectEmployees[0].Participation.role).to.equal('developer');
+      it('should let me disconnect them', function* () {
+        yield aProject.removeParticipant(anEmployee);
 
-      var employeeProjects = yield anEmployee.getParticipations();
-      expect(employeeProjects.length).to.equal(1);
-      expect(employeeProjects[0].name).to.equal('project 1');
-      expect(employeeProjects[0].Participation.role).to.equal('developer');
+        var projectParticipants = yield aProject.getParticipants();
+        expect(projectParticipants.length).to.equal(0);
+
+        var employeeParticipations = yield anEmployee.getParticipations();
+        expect(employeeParticipations.length).to.equal(0);
+
+        expect(yield Participation.count()).to.equal(0);
+        expect(yield Project.count()).to.equal(1);
+        expect(yield Employee.count()).to.equal(1);
+      });
+
+      it('should let me delete project and keep an employee', function* () {
+        yield aProject.destroy();
+        
+        var employeeParticipations = yield anEmployee.getParticipations();
+        expect(employeeParticipations.length).to.equal(0);
+
+        expect(yield Participation.count()).to.equal(0);
+        expect(yield Project.count()).to.equal(0);
+        expect(yield Employee.count()).to.equal(1);
+      });
+
+      it('should let me delete an employee and keep a project', function* () {
+        yield anEmployee.destroy();
+        
+        var projectParticipants = yield aProject.getParticipants();
+        expect(projectParticipants.length).to.equal(0);
+
+        expect(yield Participation.count()).to.equal(0);
+        expect(yield Project.count()).to.equal(1);
+        expect(yield Employee.count()).to.equal(0);
+      });
     });
   });
 });
