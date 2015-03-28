62b4a581b3f62c2671ca4e3ca3df857ccfd54942
loki2302 <loki2302@loki2302.me>
2015-03-28 12:32:20 +0300

    Cleaning up ApiService

--- fe-src/services/api/api-service.js
+++ fe-src/services/api/api-service.js
@@ -12,126 +12,127 @@ angular.module('api', [
   this.$get = ['$http', '$q', 'handle', 'errors', 'buildUri', function($http, $q, handle, errors, buildUri) {
     if(!apiRootUri) {
       throw new Error('apiRoot is not set');
-    }
+    }    
+
+    function ApiService() {};
+
+    ApiService.prototype.createNote = function(note) {
+      return handle($http.post(uri('notes'), note)).likeThis({
+        0: throwConnectivityError(),
+        201: returnData(),
+        400: throwValidationError(),
+        otherwise: throwUnexpectedError()
+      });
+    };
+
+    ApiService.prototype.updateNote = function(note) {
+      return handle($http.post(uri('notes/{id}', { id: note.id }), note)).likeThis({
+        0: throwConnectivityError(),
+        200: returnData(),
+        400: throwValidationError(),
+        404: throwNotFoundError(),
+        otherwise: throwUnexpectedError()
+      });
+    };
+
+    ApiService.prototype.deleteNote = function(note) {
+      return handle($http.delete(uri('notes/{id}', { id: note.id }), note)).likeThis({
+        0: throwConnectivityError(),
+        200: returnData(),
+        404: throwNotFoundError(),
+        otherwise: throwUnexpectedError()
+      });
+    };
+
+    ApiService.prototype.getNotes = function() {
+      return handle($http.get(uri('notes'))).likeThis({
+        0: throwConnectivityError(),
+        200: returnData(),
+        otherwise: throwUnexpectedError()
+      });
+    };
+
+    ApiService.prototype.createCategory = function(category) {
+      return handle($http.post(uri('categories'), category)).likeThis({
+        0: throwConnectivityError(),
+        201: returnData(),
+        400: throwValidationError(),
+        otherwise: throwUnexpectedError()
+      });
+    };
+
+    ApiService.prototype.updateCategory = function(category) {
+      return handle($http.post(uri('categories/{id}', { id: category.id }), category)).likeThis({
+        0: throwConnectivityError(),
+        200: returnData(),
+        400: throwValidationError(),
+        404: throwNotFoundError(),
+        otherwise: throwUnexpectedError()
+      });
+    };
+
+    ApiService.prototype.deleteCategory = function(category) {
+      return handle($http.delete(uri('categories/{id}', { id: category.id }), category)).likeThis({
+        0: throwConnectivityError(),
+        200: returnData(),
+        404: throwNotFoundError(),
+        otherwise: throwUnexpectedError()
+      });
+    };
+
+    ApiService.prototype.getCategories = function() {
+      return handle($http.get(uri('categories'))).likeThis({
+        0: throwConnectivityError(),
+        200: returnData(),
+        otherwise: throwUnexpectedError()
+      });
+    };
+
+    ApiService.prototype.getCategoriesWithNameStartingWith = function(nameStartsWith) {
+      return handle($http.get(uri('categories'), { 
+        params: { nameStartsWith: nameStartsWith } 
+      })).likeThis({
+        0: throwConnectivityError(),
+        200: returnData(),
+        otherwise: throwUnexpectedError()
+      });
+    };
 
     var uri = function(resourceTemplateString, resourceValues) {
       return buildUri(apiRootUri, resourceTemplateString, resourceValues);
     };
 
-    // TODO: somehow extract it from here
-    return new function() {
-      this.createNote = function(note) {
-        return handle($http.post(uri('notes'), note)).likeThis({
-          0: throwConnectivityError(),
-          201: returnData(),
-          400: throwValidationError(),
-          otherwise: throwUnexpectedError()
-        });
+    function throwConnectivityError() {
+      return function(httpResponse) {
+        return $q.reject(new errors.ConnectivityError());
       };
-
-      this.updateNote = function(note) {
-        return handle($http.post(uri('notes/{id}', { id: note.id }), note)).likeThis({
-          0: throwConnectivityError(),
-          200: returnData(),
-          400: throwValidationError(),
-          404: throwNotFoundError(),
-          otherwise: throwUnexpectedError()
-        });
-      };
-
-      this.deleteNote = function(note) {
-        return handle($http.delete(uri('notes/{id}', { id: note.id }), note)).likeThis({
-          0: throwConnectivityError(),
-          200: returnData(),
-          404: throwNotFoundError(),
-          otherwise: throwUnexpectedError()
-        });
-      };
-
-      this.getNotes = function() {
-        return handle($http.get(uri('notes'))).likeThis({
-          0: throwConnectivityError(),
-          200: returnData(),
-          otherwise: throwUnexpectedError()
-        });
-      };
-
-      this.createCategory = function(category) {
-        return handle($http.post(uri('categories'), category)).likeThis({
-          0: throwConnectivityError(),
-          201: returnData(),
-          400: throwValidationError(),
-          otherwise: throwUnexpectedError()
-        });
-      };
-
-      this.updateCategory = function(category) {
-        return handle($http.post(uri('categories/{id}', { id: category.id }), category)).likeThis({
-          0: throwConnectivityError(),
-          200: returnData(),
-          400: throwValidationError(),
-          404: throwNotFoundError(),
-          otherwise: throwUnexpectedError()
-        });
-      };
-
-      this.deleteCategory = function(category) {
-        return handle($http.delete(uri('categories/{id}', { id: category.id }), category)).likeThis({
-          0: throwConnectivityError(),
-          200: returnData(),
-          404: throwNotFoundError(),
-          otherwise: throwUnexpectedError()
-        });
-      };
-
-      this.getCategories = function() {
-        return handle($http.get(uri('categories'))).likeThis({
-          0: throwConnectivityError(),
-          200: returnData(),
-          otherwise: throwUnexpectedError()
-        });
-      };
-
-      this.getCategoriesWithNameStartingWith = function(nameStartsWith) {
-        return handle($http.get(uri('categories'), { 
-          params: { nameStartsWith: nameStartsWith } 
-        })).likeThis({
-          0: throwConnectivityError(),
-          200: returnData(),
-          otherwise: throwUnexpectedError()
-        });
-      };
-
-      function throwConnectivityError() {
-        return function(httpResponse) {
-          return $q.reject(new errors.ConnectivityError());
-        };
-      };
-
-      function returnData() {
-        return function(httpResponse) {
-          return httpResponse.data;
-        };
-      };
-
-      function throwValidationError() {
-        return function(httpResponse) {
-          return $q.reject(new errors.ValidationError(httpResponse.data));
-        };
-      };
-
-      function throwNotFoundError() {
-        return function(httpResponse) {
-          return $q.reject(new errors.NotFoundError());
-        };
-      };
-
-      function throwUnexpectedError() {
-        return function(httpResponse) {
-          return $q.reject(new errors.UnexpectedError());
-        };
-      };  
     };
+
+    function returnData() {
+      return function(httpResponse) {
+        return httpResponse.data;
+      };
+    };
+
+    function throwValidationError() {
+      return function(httpResponse) {
+        return $q.reject(new errors.ValidationError(httpResponse.data));
+      };
+    };
+
+    function throwNotFoundError() {
+      return function(httpResponse) {
+        return $q.reject(new errors.NotFoundError());
+      };
+    };
+
+    function throwUnexpectedError() {
+      return function(httpResponse) {
+        return $q.reject(new errors.UnexpectedError());
+      };
+    };
+
+    return new ApiService();
   }];
 })
 .value('errors', {
