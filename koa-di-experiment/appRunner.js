ab86bc0016fc95af62f93ec69dfee2ddfcd92a5a
loki2302 <loki2302@loki2302.me>
2015-03-30 08:54:49 +0300

    Cleaning up the appRunner implementation

--- koa-di-experiment/appRunner.js
+++ koa-di-experiment/appRunner.js
@@ -1,58 +1,49 @@
-function AppRunner(app, sequelize, Q, serverDestroy) {
-  this.isRunning = false;
-  this.app = app;
-  this.sequelize = sequelize;
-  this.serverDestroy = serverDestroy;
-  this.Q = Q;
-  this.server = null;
-};
-
-AppRunner.prototype.start = function() {
-  if(this.isRunning) {
-    return this.Q.reject(new Error('The application is already running'));
-  }
-
-  var self = this;
-  return this.sequelize.sync().then(function() {
-    return self.Q.Promise(function(resolve) {
-      self.server = self.app.listen(3000, function() {
-        self.serverDestroy(self.server);
-        console.log('Listening at %j', self.server.address());
-        self.isRunning = true;
-        resolve();
-      });
-    });
-  }, function(error) {
-    return this.Q.reject(new Error('Failed to initialize the database'));
-  });
-};
-
-AppRunner.prototype.stop = function() {
-  if(!this.isRunning) {
-    return this.Q.reject(new Error('The application is not running'));
-  };
-
-  var self = this;
-  return this.Q.Promise(function(resolve) {
-    self.server.destroy(function() {
-      self.server = null;
-      self.isRunning = false;
-      resolve();
-    });
-  });
-};
-
-AppRunner.prototype.reset = function() {
-  if(!this.isRunning) {
-    return this.Q.reject(new Error('The application is not running'));
-  };
-
-  var self = this;
-  return this.sequelize.drop().then(function() {
-    return self.sequelize.sync();
-  });
-};
-
 module.exports = function(app, sequelize, Q, serverDestroy) {
-  return new AppRunner(app, sequelize, Q, serverDestroy);
+  var isRunning = false;
+  var server = null;
+
+  return {
+    start: function() {
+      if(isRunning) {
+        return Q.reject(new Error('The application is already running'));
+      }
+
+      return sequelize.sync().then(function() {
+        return Q.Promise(function(resolve) {
+          server = app.listen(3000, function() {
+            serverDestroy(server);
+            console.log('Listening at %j', server.address());
+            isRunning = true;
+            resolve();
+          });
+        });
+      }, function(error) {
+        return Q.reject(new Error('Failed to initialize the database'));
+      });
+    },
+
+    stop: function() {
+      if(!isRunning) {
+        return Q.reject(new Error('The application is not running'));
+      };
+
+      return Q.Promise(function(resolve) {
+        server.destroy(function() {
+          server = null;
+          isRunning = false;
+          resolve();
+        });
+      });
+    },
+
+    reset: function() {
+      if(!isRunning) {
+        return Q.reject(new Error('The application is not running'));
+      };
+
+      return sequelize.drop().then(function() {
+        return sequelize.sync();
+      });
+    }
+  };
 };
