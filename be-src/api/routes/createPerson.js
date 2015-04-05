2a8fe713b713822e798732582ea9d3b5a6c92805
loki2302 <loki2302@loki2302.me>
2015-04-05 19:05:21 +0300

    Cleaning up

--- be-src/api/routes/createPerson.js
+++ be-src/api/routes/createPerson.js
@@ -16,14 +16,6 @@ module.exports = function(Person, PersonMembershipsRelation, Sequelize) {
         throw e;
       }
 
-      //
-      // http://stackoverflow.com/questions/26324142/rest-api-what-should-my-post-return
-      // this.set({
-      //   'Location': '/api/people/123'
-      // })
-      // ????
-      //
-
       person = yield Person.find({
         where: { id: person.id },
         include: [{ association: PersonMembershipsRelation }]
