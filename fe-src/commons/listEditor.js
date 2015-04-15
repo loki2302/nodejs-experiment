207c3228ce4651a2e1c8c2ad4d30170b0ae150c3
loki2302 <loki2302@loki2302.me>
2015-04-15 23:49:34 +0300

    Cleaning up

--- fe-src/commons/listEditor.js
+++ fe-src/commons/listEditor.js
@@ -15,16 +15,18 @@ angular.module('tbListEditor', [])
 
       scope[attrs.as] = {
         removeItem: function(item) {
-          var itemIndex = $parse(attrs.for)(scope).indexOf(item);
+          var items = scope.$eval(collectionExpression);
+          var itemIndex = items.indexOf(item);
           if(itemIndex < 0) {
             throw new Error('Did not find an item');
           }
 
-          $parse(attrs.for)(scope).splice(itemIndex, 1);
+          items.splice(itemIndex, 1);
         },
         newItem: {},
         addItem: function() {
-          $parse(attrs.for)(scope).push(scope[attrs.as].newItem);
+          var items = scope.$eval(collectionExpression);
+          items.push(scope[attrs.as].newItem);
           scope[attrs.as].newItem = {};
         }
       };
