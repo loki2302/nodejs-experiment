8227ed7acee13689b92f2dab7b71667e214d1e73
loki2302 <loki2302@loki2302.me>
2015-04-16 09:58:30 +0300

    #66: there's now a 'loading' message while angular is loading

--- fe-src/app.js
+++ fe-src/app.js
@@ -9,4 +9,9 @@ angular.module('tbApp', [
   $locationProvider.html5Mode(true);
 }]).config(['apiServiceProvider', function(apiServiceProvider) {
   apiServiceProvider.apiRoot('/api/');
+}]).run(['$rootElement', function($rootElement) {
+  // $rootElement is <html>
+  // TODO: clean it up somehow
+  $rootElement.find('span').remove();
+  $rootElement.find('div').attr('style', '');
 }]);
