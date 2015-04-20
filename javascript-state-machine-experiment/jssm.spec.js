6f4aa9f5a46ec534c0e61c6199d2e31eb9538f0d
loki2302 <loki2302@loki2302.me>
2015-04-20 23:31:31 +0300

    Implemented a funnier test for jssm

--- javascript-state-machine-experiment/jssm.spec.js
+++ javascript-state-machine-experiment/jssm.spec.js
@@ -3,21 +3,48 @@ var expect = require('chai').expect;
 var StateMachine = require("javascript-state-machine");
 
 describe('javascript state machine', function() {
-  it('should work', function() {
-    var fsm = StateMachine.create({
-      initial: 'off',
-      events: [
-        { name: 'turnOn', from: 'off', to: 'on' },
-        { name: 'turnOff', from: 'on', to: 'off' }
-      ]
+  describe('light bulb', function() {
+    var lightBulb;
+    beforeEach(function() {
+      lightBulb = StateMachine.create({
+        initial: 'off',
+        events: [
+          { name: 'turnOn', from: 'off', to: 'on' },
+          { name: 'turnOff', from: 'on', to: 'off' }
+        ]
+      });
     });
 
-    expect(fsm.current).to.equal('off');
+    it('should be off by default', function() {
+      expect(lightBulb.current).to.equal('off');
+    });
 
-    fsm.turnOn();
-    expect(fsm.current).to.equal('on');
+    it('should be possible to turn it on', function() {
+      lightBulb.turnOn();
+      expect(lightBulb.current).to.equal('on');
+    });
 
-    fsm.turnOff();
-    expect(fsm.current).to.equal('off');
+    it('should not be possible to turn it off', function() {
+      expect(function() {
+        lightBulb.turnOff();
+      }).to.throw(/event turnOff inappropriate/);
+    });
+
+    context('when it is turned on', function() {
+      beforeEach(function() {
+        lightBulb.turnOn();
+      });
+
+      it('should be possible to turn it off', function() {
+        lightBulb.turnOff();
+        expect(lightBulb.current).to.equal('off');
+      });
+
+      it('should not be possible to turn it on again', function() {
+        expect(function() {
+          lightBulb.turnOn();
+        }).to.throw(/event turnOn inappropriate/);
+      });
+    });
   });
 });
