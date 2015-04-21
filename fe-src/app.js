59a6f9ccf70f814d8cc105cf19e28d03d24c323a
loki2302 <loki2302@loki2302.me>
2015-04-21 22:13:45 +0300

    Added a dummy exception handler decorator

--- fe-src/app.js
+++ fe-src/app.js
@@ -10,6 +10,27 @@ angular.module('tbApp', [
   $locationProvider.html5Mode(true);
 }]).config(['apiServiceProvider', function(apiServiceProvider) {
   apiServiceProvider.apiRoot('/api/');
+}]).config(['$provide', function($provide) {
+  $provide.decorator('$exceptionHandler', ['$delegate', '$injector', function($delegate, $injector) {
+    return function(exception, cause) {
+      $delegate(exception, cause);
+
+      console.log('the custom exception handler says', exception, cause);
+
+      var ApiErrors = $injector.get('ApiErrors');
+      if(exception instanceof ApiErrors.ConnectivityError) {
+        console.log('interpreted as ConnectivityError');
+      } else if(exception instanceof ApiErrors.ValidationError) {
+        console.log('interpreted as ValidationError');
+      } else if(exception instanceof ApiErrors.NotFoundError) {
+        console.log('interpreted as NotFoundError');
+      } else if(exception instanceof ApiErrors.UnexpectedError) {
+        console.log('interpreted as UnexpectedError');
+      } else {
+        console.log('interpreted as have no idea');
+      }
+    };
+  }]);
 }]).run(['$rootElement', function($rootElement) {
   // $rootElement is <html>
   // TODO: clean it up somehow
