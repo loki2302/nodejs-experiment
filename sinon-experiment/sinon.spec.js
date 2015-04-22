326968b2ef7c28083f6b8990373679389922c060
loki2302 <loki2302@loki2302.me>
2015-04-22 23:12:09 +0300

    Added sinon-chai

--- sinon-experiment/sinon.spec.js
+++ sinon-experiment/sinon.spec.js
@@ -1,10 +1,22 @@
-var expect = require('chai').expect;
+var chai = require('chai');
 var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+var expect = chai.expect;
 
 describe('sinon', function() {
   it('should work', function() {
     var func = sinon.spy();
     func();
     expect(func.calledOnce).to.equal(true);
+  });
+});
+
+describe('sinon-chai', function() {
+  it('should make things more straightforward', function() {
+    var func = sinon.spy();
+    func();
+    expect(func).to.have.been.calledOnce;
   });
 });
