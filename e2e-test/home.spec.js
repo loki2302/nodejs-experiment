dce86575e53e9e90a949a39172dc57c108b1d4a0
loki2302 <loki2302@loki2302.me>
2015-04-22 09:03:50 +0300

    Added a mixed test that uses both REST API and web driver

--- e2e-test/home.spec.js
+++ e2e-test/home.spec.js
@@ -1,4 +1,5 @@
 var appRunnerFactory = require('../be-src/appRunnerFactory');
+var TeambuildrClient = require('../be-test/teambuildrClient');
 
 var HomePage = function() {
   this.teamCount = element(by.css('#teamCount'));
@@ -30,5 +31,52 @@ describe('Home', function() {
     browser.get('/');
     expect(homePage.teamCount.isPresent()).toBe(true);
     expect(homePage.personCount.isPresent()).toBe(true);
+  });
+
+  describe('stats', function() {
+    describe('when there are no teams', function() {
+      it('should say "no teams"', function() {
+        browser.get('/');
+        expect(homePage.teamCount.getText()).toBe('no teams');
+      });
+    });
+
+    describe('when there is 1 team', function() {
+      beforeEach(function(done) {
+        var client = new TeambuildrClient('http://localhost:3000/api/');
+        client.createTeam({
+          name: 'team 1',
+          url: 'http://example.org',
+          slogan: 'team 1 slogan'
+        }).finally(done);
+      });
+
+      it('should say "one team"', function() {
+        browser.get('/');
+        expect(homePage.teamCount.getText()).toBe('one team');
+      });
+    });
+
+    describe('when there are 2 teams', function() {
+      beforeEach(function(done) {
+        var client = new TeambuildrClient('http://localhost:3000/api/');
+        client.createTeam({
+          name: 'team 1',
+          url: 'http://example1.org',
+          slogan: 'team 1 slogan'
+        }).then(function() {
+          return client.createTeam({
+            name: 'team 2',
+            url: 'http://example2.org',
+            slogan: 'team 2 slogan'
+          });
+        }).finally(done);
+      });
+
+      it('should say "2 teams"', function() {
+        browser.get('/');
+        expect(homePage.teamCount.getText()).toBe('2 teams');
+      });
+    });
   });
 });
