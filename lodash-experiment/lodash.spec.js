c500e2834f889320b79f8c3e4ce7176dd650ebd4
loki2302 <loki2302@loki2302.me>
2015-04-29 10:53:54 +0300

    Added lodash/indexBy test

--- lodash-experiment/lodash.spec.js
+++ lodash-experiment/lodash.spec.js
@@ -2,9 +2,19 @@ var expect = require('chai').expect;
 var _ = require('lodash');
 
 describe('lodash', function() {
-  it('should work', function() {
+  it('should map', function() {
     expect(_.map([1, 2], function(x) {
       return x * 2;
     })).to.deep.equal([2, 4]);
+  });
+
+  it('should indexBy', function() {
+    expect(_.indexBy([
+      { id: 'one', name: 'item one' },
+      { id: 'two', name: 'item two' }
+    ], 'id')).to.deep.equal({
+      'one': { id: 'one', name: 'item one' },
+      'two': { id: 'two', name: 'item two' }
+    });
   });
 });
