0661b121c83ed95e09166f082e8290a4d128e1df
loki2302 <loki2302@loki2302.me>
2015-06-27 22:35:11 +0300

    #128: an attempt to enable E2E tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,11 +1,14 @@
+var multiCapabilities;
+if(process.env.TRAVIS) {
+  multiCapabilities = [{ browserName: 'firefox' }];
+} else {
+  multiCapabilities = [{ browserName: 'chrome' }, { browserName: 'firefox' }];
+}
+
 module.exports = {
   config: {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    multiCapabilities: [{
-      browserName: 'chrome'
-    }, {
-      browserName: 'firefox'
-    }],
+    multiCapabilities: multiCapabilities,
     maxSessions: 1,
     suites: {
       general: 'e2e-test/*.spec.js',
