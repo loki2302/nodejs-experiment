d32ec9831e1038fff48d148f520df22b7a44c369
loki2302 <loki2302@loki2302.me>
2015-07-23 22:14:53 +0300

    Added more tests

--- lunr-experiment/test.js
+++ lunr-experiment/test.js
@@ -26,4 +26,39 @@ describe('lunr', function() {
     expect(results[0].ref).to.equal('1');
     expect(results[1].ref).to.equal('2');
   });
+
+  describe('when I have tags', function() {
+    var index;
+    beforeEach(function() {
+      index = lunr(function() {
+        this.ref('id');
+        this.field('tags');
+      });
+
+      index.add({ id: '1', tags: ['milk', 'drink', 'white'] });
+      index.add({ id: '2', tags: ['beer', 'drink'] });
+    });
+
+    it('should let me find milk', function() {
+      var results = index.search('milk');
+      expect(results.length).to.equal(1);
+      expect(results[0].ref).to.equal('1');
+    });
+
+    it('should let me find beer', function() {
+      var results = index.search('beer');
+      expect(results.length).to.equal(1);
+      expect(results[0].ref).to.equal('2');
+    });
+
+    it('should let me find drinks', function() {
+      var results = index.search('drinks');
+      expect(results.length).to.equal(2);
+    });
+
+    it('should not let me find vodka', function() {
+      var results = index.search('vodka');
+      expect(results.length).to.equal(0);
+    });
+  });
 });
