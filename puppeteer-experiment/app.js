94e1340b73ba2b682b94e1edaabbd826b2bbf9c3
loki2302 <loki2302@loki2302.me>
2017-09-27 20:27:17 -0400

    Added PDF rendering and script evaluation

--- puppeteer-experiment/app.js
+++ puppeteer-experiment/app.js
@@ -8,6 +8,17 @@ const puppeteer = require('puppeteer');
         height: 2000
     });
     await page.goto('http://retask.me');
+
     await page.screenshot({ path: '1.png' });
+
+    await page.pdf({ path: '1.pdf', format: 'A4' });
+
+    const { pageTitle } = await page.evaluate(() => {
+        return {
+            pageTitle: document.title
+        };
+    });
+    console.log(`page title is ${pageTitle}`);
+
     await browser.close();
 })();
