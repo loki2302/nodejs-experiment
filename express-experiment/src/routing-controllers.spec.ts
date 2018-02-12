f1f217dddd270b2d942609d8925f63bb336e089b
agibalov <loki2302@loki2302.me>
2018-02-11 22:39:11 -0500

    Added validation test but it doesn't work

--- express-experiment/src/routing-controllers.spec.ts
+++ express-experiment/src/routing-controllers.spec.ts
@@ -2,18 +2,26 @@ import "reflect-metadata";
 
 import {expect} from 'chai';
 
-import * as express from "express";
 import * as http from "http";
 import axios, {AxiosInstance} from "axios";
 import {Body, Controller, createExpressServer, Get, JsonController, Param, Post} from "routing-controllers";
+import {IsNotEmpty, Max, Min} from "class-validator";
 
 interface ResultDto {
     result: number;
 }
 
-interface SubtractRequestDto {
+class SubtractRequestDto {
+    @Min(5)
+    @Max(20)
     a: number;
+
     b: number;
+
+    constructor(a: number, b: number) {
+        this.a = a;
+        this.b = b;
+    }
 }
 
 @JsonController('/api')
@@ -26,9 +34,9 @@ class CalculatorController {
     }
 
     @Post("/subtract")
-    async subtract(@Body() requestBody: SubtractRequestDto): Promise<ResultDto> {
+    async subtract(@Body({ required: true, validate: true }) requestBody: SubtractRequestDto): Promise<ResultDto> {
         await new Promise(resolve => {
-            setTimeout(() => resolve(), 100);
+            setTimeout(() => resolve(), 50);
         });
         return {
             result: requestBody.a - requestBody.b
@@ -42,6 +50,7 @@ describe('routing-controllers', () => {
         let client: AxiosInstance;
         before((done) => {
             const app = createExpressServer({
+                validation: true,
                 controllers: [ CalculatorController ]
             });
 
@@ -50,7 +59,8 @@ describe('routing-controllers', () => {
             });
 
             client = axios.create({
-                baseURL: 'http://localhost:3000'
+                baseURL: 'http://localhost:3000',
+                validateStatus: status => true
             });
         });
 
@@ -68,15 +78,24 @@ describe('routing-controllers', () => {
             });
         });
 
-        it('should allow me to subtract numbers', async () => {
-            const response = await client.post<ResultDto>('/api/subtract', {
-                a: 2,
+        it('should allow me to subtract numbers if request passes the validation', async () => {
+            const response = await client.post('/api/subtract', {
+                a: 7,
                 b: 3
             });
             expect(response.status).to.equal(200);
             expect(response.data).to.deep.equal({
-                result: -1
+                result: 4
             });
+        });
+
+        // Appears to be broken as of 0.7.6
+        xit('should not allow me to subtract numbers if request does not pass the validation', async () => {
+            const response = await client.post('/api/subtract', {
+                a: 2,
+                b: 3
+            });
+            expect(response.status).to.equal(400);
         });
     });
 });
