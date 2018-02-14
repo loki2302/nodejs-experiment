f6c249b5d9a010a2529ce8ec7211457d98f99e79
agibalov <loki2302@loki2302.me>
2018-02-13 22:10:55 -0500

    Added Swagger

--- nestjs-experiment/README.md
+++ nestjs-experiment/README.md
@@ -1,1 +1,3 @@
 # nestjs-experiment
+
+* Run with `npm start`. Go to `http://localhost:3000/docs` to see the Swagger page.
--- nestjs-experiment/src/index.ts
+++ nestjs-experiment/src/index.ts
@@ -1,25 +1,81 @@
+import "reflect-metadata";
 import {NestFactory} from "@nestjs/core";
-import {Controller, Get, Module} from "@nestjs/common";
+import {Body, Controller, Get, Module, Param, Post} from "@nestjs/common";
+import {ApiModelProperty, ApiOperation, ApiResponse, DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
-@Controller()
-class AppController {
-    @Get()
-    index(): string {
-        return 'Hello World!';
+export class EditableNoteAttributesDto {
+    @ApiModelProperty({
+        description: 'Note text'
+    })
+    readonly text: string;
+}
+
+export class NoteDto extends EditableNoteAttributesDto {
+    @ApiModelProperty({
+        description: 'Note ID'
+    })
+    readonly id: string;
+}
+
+@Controller('/notes')
+export class NotesController {
+    @ApiOperation({
+        title: 'Create a note',
+        description: 'Yes, create a note.'
+    })
+    @ApiResponse({
+        status: 201,
+        description: 'Successfully created a note'
+    })
+    @ApiResponse({
+        status: 400,
+        description: 'Something is wrong with your request'
+    })
+    @Post()
+    createNote(@Body() editableNoteAttributesDto: EditableNoteAttributesDto) {
+        // TODO
+    }
+
+    @ApiOperation({
+        title: 'Get a note',
+        description: 'Get a note definitely.'
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'Found a note, providing its contents',
+        type: NoteDto
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'Did not find the note'
+    })
+    @Get(':id')
+    getNote(@Param('id') id: string): NoteDto {
+        // TODO
+        return new NoteDto();
     }
 }
 
 @Module({
     imports: [],
     controllers: [
-        AppController
+        NotesController
     ],
     components: []
 })
-class AppModule {
+export class AppModule {
 }
 
 (async () => {
     const app = await NestFactory.create(AppModule);
+
+    const options = new DocumentBuilder()
+        .setTitle('Notes API')
+        .setDescription('Use this API to work with notes.')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('/docs', app, document);
+
     await app.listen(3000);
 })();
