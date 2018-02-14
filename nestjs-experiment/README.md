77bcdd26b7816e9fc965e2e18248c75c2097c2cd
agibalov <loki2302@loki2302.me>
2018-02-13 23:24:57 -0500

    Added a static page

--- nestjs-experiment/README.md
+++ nestjs-experiment/README.md
@@ -1,3 +1,5 @@
 # nestjs-experiment
 
-* Run with `npm start`. Go to `http://localhost:3000/docs` to see the Swagger page.
+* Run with `npm start`. 
+* Go to `http://localhost:3000/` to see the static page.
+* Go to `http://localhost:3000/docs` to see the Swagger page.
--- nestjs-experiment/src/index.ts
+++ nestjs-experiment/src/index.ts
@@ -4,19 +4,25 @@ import {NestFactory} from "@nestjs/core";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 import {MyValidationPipe} from "./my-validation.pipe";
 import {AppModule} from "./app.module";
+import * as express from "express";
 
 (async () => {
-    const app = await NestFactory.create(AppModule);
+    const app = express();
 
+    app.use(express.static(__dirname + '/static'));
+
+    const nestApp = await NestFactory.create(AppModule, app);
     const options = new DocumentBuilder()
         .setTitle('Notes API')
         .setDescription('Use this API to work with notes.')
         .setVersion('1.0')
         .build();
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('/docs', app, document);
+    const document = SwaggerModule.createDocument(nestApp, options);
+    SwaggerModule.setup('/docs', nestApp, document);
+    nestApp.useGlobalPipes(new MyValidationPipe());
 
-    app.useGlobalPipes(new MyValidationPipe());
-
-    await app.listen(3000);
+    const port = 3000;
+    await nestApp.listen(port, () => {
+        console.log(`Listening at port ${port}`);
+    });
 })();
